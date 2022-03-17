5e07252740047da8f8dd4d7e953193c9c1c22348
Yudi <renanyaguinuma@hotmail.com>
2022-03-17 14:22:03 -0300

    Add Movimento estudantil

--- src/app/shared/services/courses.service.ts
+++ src/app/shared/services/courses.service.ts
@@ -3,6 +3,10 @@ export class CoursesService {
     a1: {
       name: 'Para todos os cursos',
     },
+    a2: {
+      name: 'Movimento estudantil',
+      color: '#ff6b6b',
+    },
     '12': {
       name: 'Ciência da computação',
       color: '#51aaf2',
