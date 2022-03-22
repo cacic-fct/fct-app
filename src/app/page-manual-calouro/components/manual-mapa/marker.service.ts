5558b20ca99ca2cce9c9fd720110141888d31cc8
Yudi <renanyaguinuma@hotmail.com>
2022-03-21 21:23:05 -0300

    wip

--- src/app/page-manual-calouro/components/manual-mapa/marker.service.ts
+++ src/app/page-manual-calouro/components/manual-mapa/marker.service.ts
@@ -1,7 +1,11 @@
+import Map from 'ol/Map';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-import * as L from 'leaflet';
+import GeoJSON from 'ol/format/GeoJSON';
+import { Icon, Style } from 'ol/style';
+import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer';
+import VectorSource from 'ol/source/Vector';
+import Overlay from 'ol/Overlay';
 
 @Injectable({
   providedIn: 'root',
@@ -9,26 +13,24 @@ import * as L from 'leaflet';
 export class MarkerService {
   layers: Array<string> = ['mercados', 'compras'];
 
-  mercadosIcon = new L.Icon({
-    iconUrl:
-      'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-    shadowUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
+  mercadosIcon = new Style({
+    image: new Icon({
+      anchor: [0.5, 1],
+      scale: 0.5,
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      src: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+    }),
   });
 
-  comprasIcon = new L.Icon({
-    iconUrl:
-      'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
-    shadowUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
+  comprasIcon = new Style({
+    image: new Icon({
+      anchor: [0.5, 1],
+      scale: 0.5,
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      src: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png',
+    }),
   });
 
   mercadosStyle = {
@@ -41,38 +43,44 @@ export class MarkerService {
 
   constructor(private http: HttpClient) {}
 
-  makeGroceriesMarkers(map: L.Map): void {
+  // For each layer create a geojson layer with markers and polygons.
+  // Mercados style is mercadosIcon and mercadosStyle.
+  // Compras style is comprasIcon and comprasStyle.
+  // bindPopup of markers and polygons to "<b>feature.properties.name</b><br>feature.properties.description"
+
+  makeGroceriesMarkers(map: Map): void {
     // For each layer create a geojson layer with markers and polygons.
     // Mercados style is mercadosIcon and mercadosStyle.
     // Compras style is comprasIcon and comprasStyle.
     // bindPopup of markers and polygons to "<b>feature.properties.name</b><br>feature.properties.description"
     this.layers.forEach((layer) => {
-      this.http
-        .get(
-          `https://cdn.jsdelivr.net/gh/cacic-fct/manual-do-calouro@main/map/${layer}.geojson`
-        )
-        .subscribe((data: any) => {
-          const geojson = L.geoJSON(data, {
-            pointToLayer: (feature, latlng) => {
-              return L.marker(latlng, {
-                icon: this[layer + 'Icon'],
-              });
-            },
-            style: this[layer + 'Style'],
-            onEachFeature: (feature, layer) => {
-              if (feature.properties) {
-                layer.bindPopup(
-                  L.popup().setContent(
-                    `<b>${feature.properties.name}</b><br>${
-                      feature.properties.description || ''
-                    }`
-                  )
-                );
-              }
-            },
-          });
-          geojson.addTo(map);
-        });
+      const vectorSource = new VectorSource({
+        url: `https://cdn.jsdelivr.net/gh/cacic-fct/manual-do-calouro@main/map/${layer}.geojson`,
+        format: new GeoJSON(),
+      });
+
+      const vectorLayer = new VectorLayer({
+        source: vectorSource,
+        style: (feature) => {
+          if (layer === 'mercados') {
+            return this.mercadosIcon;
+          } else {
+            return this.comprasIcon;
+          }
+        },
+      });
+
+      // popup overlay
+      const popup = new Overlay({
+        element: document.getElementById('popup'),
+        autoPan: true,
+        autoPanAnimation: {
+          duration: 250,
+        },
+      });
+      map.addOverlay(popup);
+
+      map.addLayer(vectorLayer);
     });
   }
 }
--- src/app/page-manual-calouro/page-manual-calouro.page.ts
+++ src/app/page-manual-calouro/page-manual-calouro.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { useGeographic } from 'ol/proj';
+
+import { MarkerService } from './components/manual-mapa/marker.service';
 
 @Component({
   selector: 'app-page-manual-calouro',
@@ -6,11 +13,30 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   styleUrls: ['./page-manual-calouro.page.scss'],
 })
 export class PageManualCalouroPage implements OnInit, AfterViewInit {
-  constructor() {}
+  public map: Map;
+  constructor(private markerService: MarkerService) {}
 
   ngAfterViewInit(): void {}
 
-  ionViewWillEnter() {}
+  ionViewWillEnter() {
+    useGeographic();
+    const rasterLayer = new TileLayer({
+      source: new OSM(),
+    });
+
+    this.map = new Map({
+      view: new View({
+        center: [-51.40775, -22.12103],
+        zoom: 18,
+        maxZoom: 19,
+        projection: 'EPSG:3857',
+      }),
+      layers: [rasterLayer],
+      target: 'ol-map-manual',
+    });
+
+    this.markerService.makeGroceriesMarkers(this.map);
+  }
 
   ionViewDidEnter() {}
 
