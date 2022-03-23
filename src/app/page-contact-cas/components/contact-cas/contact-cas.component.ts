7202cd838d245550b4958c9df814bc2013855c34
willshobwish <willianyos13@gmail.com>
2022-03-23 14:23:12 -0300

    Criar o modulo da página
    Criar o modulo da página do contato dos centros acadêmicos

---
+++ src/app/page-contact-cas/components/contact-cas/contact-cas.component.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-cas',
+  templateUrl: './contact-cas.component.html',
+  styleUrls: ['./contact-cas.component.scss'],
+})
+export class ContactCasComponent implements OnInit {
+  contacts: any = [
+    {
+      name: 'Ciência da Computação',
+      acronym: 'CACiC',
+      social: {
+        facebook: 'cacic.fct',
+        instagram: 'cacic.fct',
+        youtube: 'UCoYH_4dyIQHx2Kad-HG6aNA',
+      },
+    },
+    {
+      name: 'Arquitetura e Urbanismo',
+      acronym: 'CACAU',
+      social: {
+        facebook: '',
+        instagram: 'cacau.unespp',
+      },
+    },
+    {
+      name: 'Educação Física',
+      acronym: 'CAEF',
+      social: {
+        facebook: '',
+        instagram: 'caef_fct',
+      },
+    },
+    {
+      name: 'Engenharia Ambiental',
+      acronym: 'CAEA',
+      social: {
+        facebook: '',
+        instagram: 'caengambiental',
+      },
+    },
+    {
+      name: 'Engenharia Cartográfica e de Agrimensura',
+      acronym: 'CAMA',
+      social: {
+        facebook: '',
+        instagram: 'cacartografica',
+      },
+    },
+    {
+      name: 'Estatística',
+      acronym: 'CAE',
+      social: {
+        facebook: '',
+        instagram: 'cae_unesp',
+      },
+    },
+    // {
+    //   name: 'Física',
+    //   acronym: '',
+    //   social: {
+    //     facebook: '',
+    //     instagram: '',
+    //   },
+    // },
+    {
+      name: 'Fisioterapia',
+      acronym: 'CAF',
+      social: {
+        facebook: '',
+        instagram: 'caf.unesp',
+      },
+    },
+    {
+      name: 'Geografia',
+      acronym: 'CAGEO',
+      social: {
+        facebook: '',
+        instagram: 'cageo.fctunesp',
+      },
+    },
+    {
+      name: 'Matemática',
+      acronym: 'CENTRAMA',
+      social: {
+        facebook: '',
+        instagram: '',
+      },
+    },
+    {
+      name: 'Pedagogia',
+      acronym: '',
+      social: {
+        facebook: '',
+        instagram: '',
+      },
+    },
+    {
+      name: 'Química',
+      acronym: '',
+      social: {
+        facebook: '',
+        instagram: '',
+      },
+    },
+  ];
+  socialMedia = {
+    linkedin: 'https://linkedin.com/in/',
+    twitter: 'https://twitter.com/',
+    facebook: 'https://fb.com/',
+    instagram: 'https://instagram.com/',
+    youtube: 'https://youtube.com/channel/',
+    medium: 'https://medium.com/@',
+    flickr: 'https://flickr.com/photos/',
+    twitch: 'https://twitch.tv/',
+    soundcloud: 'https://soundcloud.com/',
+    spotify: 'https://open.spotify.com/user/',
+    reddit: 'https://reddit.com/u/',
+    deviantart: 'https://deviantart.com/',
+    steam: 'https://steamcommunity.com/id/',
+    xbox: 'http://live.xbox.com/Profile?Gamertag=',
+  };
+  constructor() {}
+
+  ngOnInit() {}
+}
