46530f0c2fa0d7c98f67dfe1b1369f318c2c9dc4
Guilherme Tomiasi <gc.tomiasi@unesp.br>
2022-04-15 14:15:46 -0300

    importing page-attendance-list-page-routing module

--- src/app/page-restricted-area/shared/components/event-list/event-list.module.ts
+++ src/app/page-restricted-area/shared/components/event-list/event-list.module.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { PageAttendanceListPageRoutingModule } from 'src/app/page-restricted-area/page-attendance-list/page-attendance-list-routing.module';
 import { NgModule } from '@angular/core';
 import { EventListComponent } from './event-list.component';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule
+    IonicModule,
+    PageAttendanceListPageRoutingModule,
   ],
   declarations: [
     EventListComponent,
