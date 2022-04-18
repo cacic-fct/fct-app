6fb7bdf1e1c6b2f504d0384275fda147b2e89704
Yudi <renanyaguinuma@hotmail.com>
2022-04-17 21:53:54 -0300

    Return if undefined

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -80,6 +80,9 @@ export class AuthService {
       .doc<User>(`users/${user.uid}`)
       .valueChanges()
       .subscribe((data) => {
+        if (data === undefined) {
+          return;
+        }
         if (!data.dataVersion || data.dataVersion !== this.dataVersion) {
           this.router.navigate(['/register']);
         }
