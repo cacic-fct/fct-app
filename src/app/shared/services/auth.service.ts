1aafc8713934a47f03f3ef4a76ff98b201e4beb9
Yudi <renanyaguinuma@hotmail.com>
2022-04-22 18:58:45 -0300

    Revert register fixes

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -80,7 +80,7 @@ export class AuthService {
       .doc<User>(`users/${user.uid}`)
       .valueChanges()
       .subscribe((data) => {
-        if (data === undefined || data?.dataVersion === undefined) {
+        if (data === undefined) {
           return;
         }
         if (!data.dataVersion || data.dataVersion !== this.dataVersion) {
