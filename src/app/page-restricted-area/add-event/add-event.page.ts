8ba07adf3cb1975506fbd9d607cbd031661e5f3f
Guilherme Tomiasi <gc.tomiasi@unesp.br>
2022-04-22 19:58:35 -0300

    initial form model for addEvent

--- src/app/page-restricted-area/add-event/add-event.page.ts
+++ src/app/page-restricted-area/add-event/add-event.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CoursesService } from 'src/app/shared/services/courses.service';
+import { format, parseISO, getDate, getMonth, getYear } from 'date-fns';
 
 @Component({
   selector: 'app-add-event',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.page.scss'],
 })
 export class AddEventPage implements OnInit {
+  courses = CoursesService.courses;
+  dateValue = "";
 
+  dataForm: FormGroup = new FormGroup({
+    course: new FormControl(''),
+    icon: new FormControl(''),
+    name: new FormControl(''),
+    shortDescription: new FormControl(''),
+    description: new FormControl(''),
+    date: new FormControl(''),
+    locationDescription: new FormControl(''),
+    locationLat: new FormControl(''),
+    locationLon: new FormControl(''),
+    youtubeCode: new FormControl(''),
+    public: new FormControl('on'),
+    buttonText: new FormControl(''),
+    buttonUrl: new FormControl(''),
+  });
   constructor() { }
 
   ngOnInit() {
   }
 
+  formatDate(value: string) {
+    return format(parseISO(value), 'dd/MM/yyyy HH:mm');
+  }
+
+  onSubmit() {}
 }
