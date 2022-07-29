6b0e01be48466ba4c28544a420c344992b05147b
Yudi <renanyaguinuma@hotmail.com>
2022-07-28 21:22:53 -0300

    wip

--- src/app/page-restricted-area/add-major-event/add-major-event.page.ts
+++ src/app/page-restricted-area/add-major-event/add-major-event.page.ts
@@ -9,6 +9,7 @@ import { parse as parseDate } from 'date-fns';
 import { DomSanitizer } from '@angular/platform-browser';
 import { parse } from 'twemoji-parser';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-add-major-event',
@@ -36,13 +37,15 @@ export class AddMajorEventPage implements OnInit {
     dateStart: new FormControl(''),
     dateEnd: new FormControl(''),
     price: new FormControl(''),
-    priceDiferentiate: new FormControl(''),
     priceStudents: new FormControl(''),
     priceOtherStudents: new FormControl(''),
     priceProfessors: new FormControl(''),
     accountChavePix: new FormControl(''),
     accountBank: new FormControl(''),
     accountName: new FormControl(''),
+    accountDocument: new FormControl(''),
+    accountAgency: new FormControl(''),
+    accountNumber: new FormControl(''),
     public: new FormControl(''),
     buttonText: new FormControl(''),
     buttonUrl: new FormControl(''),
@@ -52,7 +55,8 @@ export class AddMajorEventPage implements OnInit {
   constructor(
     public formBuilder: FormBuilder,
     private modalController: ModalController,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private afs: AngularFirestore
   ) {
     this.userData = JSON.parse(localStorage.getItem('user'));
   }
@@ -68,19 +72,21 @@ export class AddMajorEventPage implements OnInit {
         dateStart: ['', Validators.required],
         dateEnd: '',
         price: '',
-        priceDiferentiate: '',
         priceStudents: '',
         priceOtherStudents: '',
         priceProfessors: '',
         accountChavePix: '',
         accountBank: '',
         accountName: '',
+        accountDocument: '',
+        accountAgency: '',
+        accountNumber: '',
         public: '',
         buttonText: '',
         buttonUrl: '',
       },
       {
-        validators: [this.validatorButton, this.validatorPix],
+        validators: [this.validatorButton, this.requirePaymentDetails],
       }
     );
     this.userData.displayName.replace(/%20/g, ' ');
@@ -91,8 +97,37 @@ export class AddMajorEventPage implements OnInit {
   }
 
   onSubmit() {
-    console.log('submit');
-  } // TODO implementar
+    if (!this.dataForm.valid) {
+      return false;
+    }
+    this.afs.collection('majorEvents').add({
+      course: this.dataForm.get('course').value,
+      icon: this.dataForm.get('icon').value,
+      name: this.dataForm.get('name').value,
+      shortDescription: this.dataForm.get('shortDescription').value,
+      description: this.dataForm.get('description').value,
+      dateStart: this.dataForm.get('dateStart').value,
+      dateEnd: this.dataForm.get('dateEnd').value,
+      price: this.dataForm.get('price').value
+        ? this.dataForm.get('price').value
+        : {
+            priceStudents: this.dataForm.get('priceStudents').value,
+            priceOtherStudents: this.dataForm.get('priceOtherStudents').value,
+            priceProfessors: this.dataForm.get('priceProfessors').value,
+          },
+      accountChavePix: this.dataForm.get('accountChavePix').value,
+      accountBank: this.dataForm.get('accountBank').value,
+      accountName: this.dataForm.get('accountName').value,
+      accountDocument: this.dataForm.get('accountDocument').value,
+      accountAgency: this.dataForm.get('accountAgency').value,
+      accountNumber: this.dataForm.get('accountNumber').value,
+      public: this.dataForm.get('public').value,
+      buttonText: this.dataForm.get('buttonText').value,
+      buttonUrl: this.dataForm.get('buttonUrl').value,
+      createdBy: this.userData.displayName,
+      createdAt: new Date(),
+    });
+  }
 
   validatorButton(control: AbstractControl): ValidationErrors | null {
     if (control.get('buttonText').value != '') control.get('buttonUrl').addValidators(Validators.required);
@@ -101,13 +136,19 @@ export class AddMajorEventPage implements OnInit {
     return null;
   }
 
-  validatorPix(control: AbstractControl): ValidationErrors | null {
-    if (control.get('accountChavePix').value != '') {
+  requirePaymentDetails(control: AbstractControl): ValidationErrors | null {
+    if (control.get('accountChavePix').value != '' || control.get('accountNumber').value != '') {
       control.get('accountBank').addValidators(Validators.required);
       control.get('accountName').addValidators(Validators.required);
+      control.get('accountDocument').addValidators(Validators.required);
+      control.get('accountAgency').addValidators(Validators.required);
+      control.get('accountNumber').addValidators(Validators.required);
     } else {
       control.get('accountBank').removeValidators(Validators.required);
       control.get('accountName').removeValidators(Validators.required);
+      control.get('accountDocument').addValidators(Validators.required);
+      control.get('accountAgency').addValidators(Validators.required);
+      control.get('accountNumber').addValidators(Validators.required);
     }
     return null;
   }
