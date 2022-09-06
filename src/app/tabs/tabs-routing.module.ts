501b90c039528434bf93cbb14b98e502680454c1
Yudi <renanyaguinuma@hotmail.com>
2022-09-06 16:48:46 -0300

    Add page title

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -31,6 +31,7 @@ const routes: Routes = [
       },
       {
         path: 'eventos',
+        title: 'Lista de eventos',
         loadChildren: () => import('../tab-events/tab-events.module').then((m) => m.TabEventsPageModule),
       },
       {
