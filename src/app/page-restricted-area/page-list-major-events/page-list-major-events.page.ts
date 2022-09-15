59860cd893c5b34182cb213c4ff15310353c6914
Bruno Augusto Furquim <97642457+BrunoAFurquim@users.noreply.github.com>
2022-09-15 14:52:05 -0300

    show events from firestore

--- src/app/page-restricted-area/page-list-major-events/page-list-major-events.page.ts
+++ src/app/page-restricted-area/page-list-major-events/page-list-major-events.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { startOfMonth, endOfMonth, addDays, format, parseISO } from 'date-fns'
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
+export interface Evento { content: string; course: string; date: Date; name: String; }
 
 @Component({
   selector: 'app-page-list-major-events',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-list-major-events.page.scss'],
 })
 export class PageListMajorEventsPage implements OnInit {
+  itemsCollection: AngularFirestoreCollection<Evento>;
+  item: Evento = { content: '', course: '', date: new Date(), name: '' }
+  currentDay: String = format(new Date(), 'dd/MM/yyyy');
+  limitDate: String = format(addDays(new Date(), 365), 'dd/MM/yyyy');
+  eventsArray: Evento[] = [];
+  eventsDates: string[] = [];
+  items: Observable<Evento[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Evento>('events');
+    this.items = this.itemsCollection.valueChanges();
   }
 
+  async ngOnInit() {
+    let events = await this.itemsCollection.ref.where('date', '<', endOfMonth(new Date)).where('date', '>', startOfMonth(new Date)).get()
+    console.log(events.size)
+
+    events.docs.forEach(doc => { this.eventsArray.push(this.item = doc.data()) })
+
+    //events.docs.forEach(doc => { this.eventsDates.push(format(this.item.date, 'dd/MM/yyyy'))})
+    //console.log(this.eventsDates);
+
+    /* for (let i = 0; i < this.eventsArray.length; i++) {
+      this.eventsDates[i] = this.eventsArray[i].date.toLocaleString();
+      console.log(format(this.eventsDates[i].date, 'dd/MM/yyyy'));
+      console.log(this.eventsDates[i]);
+    } */
+  }
+
+  /* formatDate(value: string) {
+    return format(parseISO(value), 'dd/MM/yyyy HH:mm');
+  } */
+
 }
+
+// firebase emulators:start --project fct-pp --import=./emulator-data --export-on-exit
