2186633ebd471ee8fa74b1e543dc32657fb0ca3b
Yudi <renanyaguinuma@hotmail.com>
2022-09-15 18:39:30 -0300

    Month selector

--- src/app/page-restricted-area/page-list-major-events/page-list-major-events.page.ts
+++ src/app/page-restricted-area/page-list-major-events/page-list-major-events.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { startOfMonth, endOfMonth, addDays, format, parseISO, fromUnixTime } from 'date-fns';
+import { Component } from '@angular/core';
+import { startOfMonth, endOfMonth, format, parseISO, fromUnixTime, addYears } from 'date-fns';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, switchMap } from 'rxjs';
 import { MajorEventItem } from 'src/app/shared/services/major-event';
 import { Timestamp } from '@firebase/firestore-types';
 import { trace } from '@angular/fire/compat/performance';
@@ -12,35 +12,39 @@ import { CoursesService } from 'src/app/shared/services/courses.service';
   templateUrl: './page-list-major-events.page.html',
   styleUrls: ['./page-list-major-events.page.scss'],
 })
-export class PageListMajorEventsPage implements OnInit {
-  itemsCollection: AngularFirestoreCollection<MajorEventItem>;
-  item: MajorEventItem;
-  currentDay: string = new Date().toISOString();
+export class PageListMajorEventsPage {
+  today: Date = new Date();
+  currentMonth: string = this.today.toISOString();
+  currentMonth$: BehaviorSubject<string | null> = new BehaviorSubject(this.currentMonth);
+  limitDate: string = format(addYears(this.today, 1), 'dd/MM/yyyy');
   majorEvents$: Observable<MajorEventItem[]>;
 
   constructor(private afs: AngularFirestore, public courses: CoursesService) {
-    this.majorEvents$ = this.afs
-      .collection<MajorEventItem>('majorEvents', (ref) => {
-        let query: any = ref;
-        query = query
-          .where('eventStartDate', '<=', endOfMonth(parseISO(this.currentDay)))
-          .where('eventStartDate', '>=', startOfMonth(parseISO(this.currentDay)));
-        console.log(parseISO(this.currentDay));
-        return query;
+    this.majorEvents$ = combineLatest([this.currentMonth$]).pipe(
+      switchMap(([date]) => {
+        return this.afs
+          .collection<MajorEventItem>('majorEvents', (ref) => {
+            let query: any = ref;
+            query = query
+              .where('eventStartDate', '<=', endOfMonth(parseISO(date)))
+              .where('eventStartDate', '>=', startOfMonth(parseISO(date)));
+            return query;
+          })
+          .valueChanges({ idField: 'id' })
+          .pipe(trace('firestore'));
       })
-      .valueChanges({ idField: 'id' })
-      .pipe(trace('firestore'));
+    );
   }
-
-  ngOnInit() {}
 
   getDateFromTimestamp(timestamp: Timestamp): Date {
     return fromUnixTime(timestamp.seconds);
   }
 
   getLimitDate(): string {
-    return addDays(new Date(), 365).toISOString();
+    return addYears(this.today, 1).toISOString();
+  }
+
+  onMonthChange() {
+    this.currentMonth$.next(this.currentMonth);
   }
 }
-
-// firebase emulators:start --project fct-pp --import=./emulator-data --export-on-exit
