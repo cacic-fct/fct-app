d44db1a73257e37ef7c2109c840c44e0eae2923d
Murilo Pereira <contato@murilopereira.dev.br>
2022-09-23 12:35:25 -0300

    feat: Adicionado envio de comprovante

--- src/app/page-payment/page-pay/page-pay.page.ts
+++ src/app/page-payment/page-pay/page-pay.page.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { Observable, first } from 'rxjs';
+import { Observable, first, Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { DataUrl, NgxImageCompressService } from 'ngx-image-compress';
 import { ToastController } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { trace } from '@angular/fire/compat/performance';
+import { format, parseISO, addHours } from 'date-fns';
 
 @UntilDestroy()
 @Component({
@@ -19,15 +22,18 @@ export class PagePayPage implements OnInit {
   downloadURL: Observable<string>;
   uid: string;
   fileName: string;
-
+  majorEvent: any;
+  enrollment: any;
   eventID: string;
+  rawFile: any;
 
   constructor(
     private storage: AngularFireStorage,
     public auth: AngularFireAuth,
     private imageCompress: NgxImageCompressService,
     public toastController: ToastController,
-    private router: Router
+    private router: Router,
+    public firestore: AngularFirestore
   ) {
     this.eventID = this.router.url.split('/')[3];
   }
@@ -36,8 +42,32 @@ export class PagePayPage implements OnInit {
     this.auth.user.pipe(first()).subscribe((user) => {
       if (user) {
         this.uid = user.uid;
+        this.firestore
+          .collection(`/users/${user.uid}/majorEventEnrollments`)
+          .doc<any>(this.eventID)
+          .valueChanges()
+          .subscribe((value) => {
+            return (this.enrollment = value);
+          });
       }
     });
+
+    this.firestore
+      .collection('majorEvents')
+      .doc<any>(this.eventID)
+      .valueChanges()
+      .subscribe((value) => {
+        return (this.majorEvent = {
+          ...value,
+          subscriptionStartDate: format(parseISO(value.subscriptionStartDate), 'dd/MM/yyyy HH:mm'),
+          subscriptionEndDate: format(parseISO(value.subscriptionEndDate), 'dd/MM/yyyy HH:mm'),
+        });
+      });
+  }
+
+  copyToClipboard() {
+    navigator.clipboard.writeText(this.majorEvent.accountChavePix);
+    alert('Chave copiada para área de transferência');
   }
 
   compressFile() {
@@ -45,12 +75,15 @@ export class PagePayPage implements OnInit {
     this.imageCompress.uploadAndGetImageWithMaxSize(MAX_MEGABYTE).then(
       (result: string) => {
         this.uploadFile(result);
+        this.rawFile = result;
       },
       (result: string) => {
         if (this.imageCompress.byteCount(result) > 10_000_000) {
           this.toastSize();
           return;
         }
+
+        this.rawFile = result;
         this.uploadFile(result);
       }
     );
@@ -86,8 +119,32 @@ export class PagePayPage implements OnInit {
 
     task
       .snapshotChanges()
-      .pipe(finalize(() => ((this.downloadURL = fileRef.getDownloadURL()), untilDestroyed(this))))
+      .pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.updateUser();
+        }),
+        untilDestroyed(this)
+      )
       .subscribe();
+  }
+
+  updateUser() {
+    this.firestore
+      .doc<any>(`/users/${this.uid}/majorEventEnrollments/${this.eventID}`)
+      .update({ receiptUploaded: true, receiptLink: this.downloadURL })
+      .then(() => {
+        this.toastSuccess();
+
+        setTimeout(() => {
+          this.toastController.dismiss();
+          this.router.navigate(['/pagamentos'], { replaceUrl: true });
+        }, 1500);
+      })
+      .catch((err) => {
+        this.toastError();
+        console.error('Failed to update user', err);
+      });
   }
 
   async toastError() {
@@ -113,6 +170,25 @@ export class PagePayPage implements OnInit {
       header: 'Falha no upload',
       message: 'O arquivo escolhido é muito grande. Tente novamente com um arquivo menor.',
       icon: 'close-circle',
+      position: 'bottom',
+      duration: 5000,
+      buttons: [
+        {
+          side: 'end',
+          text: 'OK',
+          role: 'cancel',
+        },
+      ],
+    });
+    toast.present();
+  }
+
+  async toastSuccess() {
+    const toast = await this.toastController.create({
+      header: 'Comprovante enviado',
+      message:
+        'O comprovante foi enviado com sucesso! Acompanhe o status do pagamento através da seção "Minhas Inscrições"',
+      icon: 'checkmark-circle',
       position: 'bottom',
       duration: 5000,
       buttons: [
