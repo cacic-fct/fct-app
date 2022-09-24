a9e40249403b5fb0007e82a136a7eb7714e68f6c
Yudi <renanyaguinuma@hotmail.com>
2022-09-24 16:20:20 -0300

    Display professor or external status

--- src/app/page-profile/page-profile.page.ts
+++ src/app/page-profile/page-profile.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { CoursesService } from '../shared/services/courses.service';
 
-import { first, Observable, map } from 'rxjs';
+import { first, Observable, map, BehaviorSubject } from 'rxjs';
 import { User } from '../shared/services/user';
 import { trace } from '@angular/fire/compat/performance';
 
@@ -18,6 +18,8 @@ export class PageProfilePage implements OnInit {
   user$: Observable<FirebaseUser>;
   academicID$: Observable<string>;
   serviceWorkerActive: boolean = false;
+  _isProfessor = new BehaviorSubject<boolean>(false);
+  isProfessor$: Observable<boolean> = this._isProfessor.asObservable();
 
   constructor(public auth: AngularFireAuth, public courses: CoursesService, private afs: AngularFirestore) {
     // If browser supports service worker
@@ -33,6 +35,12 @@ export class PageProfilePage implements OnInit {
     this.user$ = this.auth.user.pipe(first(), trace('auth'));
 
     this.user$.subscribe((user) => {
+      user.getIdTokenResult().then((idTokenResult) => {
+        if (idTokenResult.claims.role === 3000) {
+          this._isProfessor.next(true);
+        }
+      });
+
       if (user) {
         this.academicID$ = this.afs
           .doc<User>(`users/${user.uid}`)
