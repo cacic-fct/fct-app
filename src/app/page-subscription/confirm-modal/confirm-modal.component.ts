05858f3bfc5bca3913c88128f0b42b61fb3679f5
Yudi <renanyaguinuma@hotmail.com>
2022-09-26 19:34:12 -0300

    wip

--- src/app/page-subscription/confirm-modal/confirm-modal.component.ts
+++ src/app/page-subscription/confirm-modal/confirm-modal.component.ts
@@ -1,16 +1,15 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
-import { Router } from '@angular/router';
 import { Timestamp } from '@firebase/firestore-types';
 import { formatDate } from '@angular/common';
-import { fromUnixTime, format, parseISO } from 'date-fns';
+import { fromUnixTime, isSameDay } from 'date-fns';
 
 import { MajorEventItem } from '../../shared/services/major-event';
 import { EventItem } from '../../shared/services/event';
 import { ModalController } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { EnrollmentTypesService } from 'src/app/shared/services/enrollment-types.service';
 
 @Component({
   selector: 'app-confirm-modal',
@@ -24,65 +23,24 @@ export class ConfirmModalComponent implements OnInit {
   @Input() palestrasCount: number;
   @Input() subscriptionType: string;
 
-  @ViewChild('successSwal') private successSwal: SwalComponent;
-  @ViewChild('errorSwal') private errorSwal: SwalComponent;
-
-  console = console;
-  today: Date = new Date();
-
-  events$ = {};
-
-  countMinicursos$: number;
-  countPalestras$: number;
-  qntdMinicursosChecked$: string;
-  qntdPalestrasChecked$: string;
-  subscriptionTypeSelected$: string;
-  subscribedToEvents: string[] = [];
-
-  constructor(public afs: AngularFirestore, public auth: AngularFireAuth, private modalController: ModalController) {}
+  constructor(
+    public afs: AngularFirestore,
+    public auth: AngularFireAuth,
+    private modalController: ModalController,
+    public enrollmentTypes: EnrollmentTypesService
+  ) {}
 
   ngOnInit() {}
 
-  adjustNumber(num: number): string {
-    return ('00' + num).slice(-2);
-  }
-
-  countCheckeds(e: any) {
-    const { checked, name } = e.currentTarget;
-    let countMinicursos = document.querySelectorAll('[name="minicurso"][value="on"]').length;
-    let countPalestras = document.querySelectorAll('[name="palestra"][value="on"]').length;
-
-    const current = checked ? 1 : -1;
-    if (name === 'palestra') countPalestras += current;
-    else countMinicursos += current;
-  }
-
-  formatAndSaveEvent(event: EventItem) {
-    event.time = this.getIntervalHours(event.eventStartDate, event.eventEndDate);
-    const date = format(event.eventStartDate.toDate(), 'yyyy-MM-dd');
-
-    if (this.events$[date]) this.events$[date].push(event);
-    else this.events$[date] = [event];
-
-    if (event.eventType === 'minicurso') this.countMinicursos$++;
-    else this.countPalestras$++;
-  }
-
-  getIntervalHours(date1: Timestamp, date2: Timestamp): string {
-    const hour1 = format(date1.toDate(), 'HH:mm');
-    const hour2 = format(date2.toDate(), 'HH:mm');
-    return `${hour1} às ${hour2}`;
-  }
-
-  formatDate(date: string): string {
-    let formated = formatDate(parseISO(date), "EEEE, dd 'de' MMMM 'de' yyyy", 'pt-BR');
+  formatDate(date: Date): string {
+    let formated = formatDate(date, "EEEE, dd 'de' MMMM 'de' yyyy", 'pt-BR');
 
     formated = formated.charAt(0).toUpperCase() + formated.slice(1);
     return formated;
   }
 
   getDateFromTimestamp(timestamp: Timestamp): Date {
-    return fromUnixTime(parseInt(timestamp.seconds.toString()));
+    return fromUnixTime(timestamp.seconds);
   }
 
   onSubmit() {
@@ -91,5 +49,9 @@ export class ConfirmModalComponent implements OnInit {
 
   closeModal() {
     this.modalController.dismiss(false);
+  }
+
+  dayCompare(date1: Timestamp, date2: Timestamp): boolean {
+    return isSameDay(fromUnixTime(date1.seconds), fromUnixTime(date2.seconds));
   }
 }
--- src/app/page-subscription/page-subscription.page.ts
+++ src/app/page-subscription/page-subscription.page.ts
@@ -1,8 +1,10 @@
+import { EnrollmentTypesService } from './../shared/services/enrollment-types.service';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, DocumentReference } from '@angular/fire/compat/firestore';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as firestore from 'firebase/firestore';
 import { Timestamp } from '@firebase/firestore-types';
 import { formatDate } from '@angular/common';
 import { fromUnixTime, isSameDay, compareAsc } from 'date-fns';
@@ -14,6 +16,7 @@ import { ModalController, ToastController } from '@ionic/angular';
 
 import { ConfirmModalComponent } from './confirm-modal/confirm-modal.component';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { trace } from '@angular/fire/compat/performance';
 @UntilDestroy()
 @Component({
   selector: 'app-page-subscription',
@@ -21,8 +24,17 @@ import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
   styleUrls: ['page-subscription.page.scss'],
 })
 export class PageSubscriptionPage implements OnInit {
+  @ViewChild('successSwal')
+  private successSwal: SwalComponent;
+  @ViewChild('errorSwal')
+  private errorSwal: SwalComponent;
   @ViewChild('maxChanged')
   private maxChanged: SwalComponent;
+  @ViewChild('alreadySubscribed')
+  private alreadySubscribed: SwalComponent;
+  @ViewChild('eventNotFound')
+  private eventNotFound: SwalComponent;
+
   today: Date = new Date();
 
   majorEvent$: Observable<MajorEventItem>;
@@ -38,19 +50,59 @@ export class PageSubscriptionPage implements OnInit {
 
   opSelected: string;
 
+  majorEventID = this.route.snapshot.params.eventID;
+
   constructor(
     public afs: AngularFirestore,
     public auth: AngularFireAuth,
     private router: Router,
     private route: ActivatedRoute,
     private modalController: ModalController,
-    private toastController: ToastController
+    private toastController: ToastController,
+    public enrollmentTypes: EnrollmentTypesService
   ) {}
 
   ngOnInit() {
-    const id = this.route.snapshot.params.eventID;
+    // If majorEventID is not valid, redirect
+    this.afs
+      .collection('majorEvents')
+      .doc(this.majorEventID)
+      .get()
+      .pipe(untilDestroyed(this), trace('firestore'))
+      .subscribe((document) => {
+        if (!document.exists) {
+          this.eventNotFound.fire();
+          setTimeout(() => {
+            this.router.navigate(['eventos']);
+            this.eventNotFound.close();
+          }, 1000);
+        }
+      });
 
-    this.majorEvent$ = this.afs.doc<MajorEventItem>(`majorEvents/${id}`).valueChanges({ idField: 'id' });
+    // Check if user is already subscribed
+    this.auth.user.pipe(untilDestroyed(this)).subscribe((user) => {
+      if (user) {
+        this.afs
+          .doc<Subscription>(`users/${user.uid}/majorEventSubscriptions/${this.majorEventID}`)
+          .valueChanges({ idField: 'id' })
+          .pipe(untilDestroyed(this), trace('firestore'))
+          .subscribe((subscription) => {
+            if (subscription.reference) {
+              this.alreadySubscribed.fire();
+              this.router.navigate(['/eventos'], { replaceUrl: true });
+
+              setTimeout(() => {
+                this.alreadySubscribed.close();
+              }, 1000);
+            }
+          });
+      }
+    });
+
+    this.majorEvent$ = this.afs
+      .doc<MajorEventItem>(`majorEvents/${this.majorEventID}`)
+      .valueChanges({ idField: 'id' })
+      .pipe(trace('firestore'));
 
     this.events$ = this.majorEvent$.pipe(
       map((majorEvent) => {
@@ -65,14 +117,14 @@ export class PageSubscriptionPage implements OnInit {
       if (this.maxCourses && this.maxCourses !== majorEvent.maxCourses) {
         this.maxChanged.fire();
         this.router.navigateByUrl('/eventos/inscricao', { skipLocationChange: true }).then(() => {
-          this.router.navigate([`/eventos/inscricao/${id}`]);
+          this.router.navigate([`/eventos/inscricao/${this.majorEventID}`]);
         });
       }
 
       if (this.maxLectures && this.maxLectures !== majorEvent.maxLectures) {
         this.maxChanged.fire();
         this.router.navigateByUrl('/eventos/inscricao', { skipLocationChange: true }).then(() => {
-          this.router.navigate([`/eventos/inscricao/${id}`]);
+          this.router.navigate([`/eventos/inscricao/${this.majorEventID}`]);
         });
       }
 
@@ -160,57 +212,64 @@ export class PageSubscriptionPage implements OnInit {
   }
 
   onSubmit() {
+    if (
+      this.eventsSelected['minicurso'].length + this.eventsSelected['palestra'].length === 0 ||
+      this.opSelected === undefined
+    ) {
+      return;
+    }
     this.openConfirmModal().then((response) => {
       if (!response) {
         return;
       }
 
-      // this.auth.user.subscribe((user) => {
-      //   try {
-      //     this.afs
-      //       .collection(`majorEvents/${this.majorEvent$.id}/subscriptions`)
-      //       .doc(user.uid)
-      //       .set({
-      //         time: new Date(),
-      //         payment: {
-      //           status: 0,
-      //           time: new Date(),
-      //         },
-      //         subscriptionType: Number(this.subscriptionTypeSelected$),
-      //         subscribedToEvents: this.subscribedToEvents,
-      //       });
+      // Merge eventsSelected arrays
+      const eventsSelected = Object.values(this.eventsSelected).reduce((acc, val) => acc.concat(val), []);
 
-      //     this.subscribedToEvents.map((eventId) => {
-      //       this.afs.collection(`events/${eventId}/subscriptions`).doc(user.uid).set({
-      //         time: new Date(),
-      //       });
+      // Create array with event IDs from eventsSelected
+      const eventsSelectedID = eventsSelected.map((event) => event.id);
 
-      //       this.afs
-      //         .collection(`users/${user.uid}/subscriptions`)
-      //         .doc(eventId)
-      //         .set({
-      //           reference: `events/${eventId}/subscriptions/${user.uid}`,
-      //         });
-      //     });
+      const now: Timestamp = firestore.Timestamp.fromDate(new Date());
 
-      //     this.afs
-      //       .collection(`users/${user.uid}/subscriptions`)
-      //       .doc(this.majorEvent$.id)
-      //       .set({
-      //         reference: `majorEvents/${this.majorEvent$.id}/subscriptions/${user.uid}`,
-      //       });
+      this.auth.user.subscribe((user) => {
+        this.afs
+          .collection(`majorEvents/${this.majorEventID}/subscriptions`)
+          .doc(user.uid)
+          .set({
+            subscriptionType: Number.parseInt(this.opSelected),
+            subscribedToEvents: eventsSelectedID,
+            time: now,
+            payment: {
+              status: 0,
+              time: now,
+              author: user.uid,
+            },
+          })
+          .then(() => {
+            this.afs
+              .collection(`users/${user.uid}/majorEventSubscriptions`)
+              .doc(this.majorEventID)
+              .set({
+                reference: `majorEvents/${this.majorEventID}/subscriptions/${user.uid}`,
+              })
+              .then(() => {
+                this.successSwal.fire();
+                setTimeout(() => {
+                  this.successSwal.close();
+                  this.router.navigate(['/eventos'], { replaceUrl: true });
+                }, 2000);
+              });
+          })
+          .catch((error) => {
+            console.error(error);
+            this.errorSwal.fire();
+          });
 
-      //     this.successSwal.fire();
-
-      //     setTimeout(() => {
-      //       this.successSwal.close();
-      //       this.router.navigate(['/eventos'], { replaceUrl: true });
-      //     }, 1500);
-      //   } catch (error) {
-      //     this.errorSwal.fire();
-      //     console.error('Failed to write majorEvent to Firestore', error);
-      //   }
-      // });
+        setTimeout(() => {
+          this.successSwal.close();
+          this.router.navigate(['/eventos'], { replaceUrl: true });
+        }, 1500);
+      });
     });
   }
 
@@ -245,4 +304,9 @@ export class PageSubscriptionPage implements OnInit {
       });
     });
   }
+}
+
+interface Subscription {
+  id?: string;
+  reference?: DocumentReference<any>;
 }
