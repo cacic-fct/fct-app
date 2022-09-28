8d0ef03f938ddf1c5593958059c86230cb23b585
Yudi <renanyaguinuma@hotmail.com>
2022-09-28 14:14:57 -0300

    Use this.today instead of const now = new Date()

--- src/app/page-subscriptions/page-subscriptions.page.ts
+++ src/app/page-subscriptions/page-subscriptions.page.ts
@@ -20,6 +20,8 @@ import { EnrollmentTypesService } from '../shared/services/enrollment-types.serv
 export class PageSubscriptionsPage implements OnInit {
   subscriptions$: Observable<Subscription[]>;
 
+  today: Date = new Date();
+
   constructor(
     public afs: AngularFirestore,
     public auth: AngularFireAuth,
@@ -56,45 +58,27 @@ export class PageSubscriptionsPage implements OnInit {
   getDateFromTimestamp(timestamp: Timestamp): Date {
     return fromUnixTime(timestamp.seconds);
   }
-  isInTheSubscriptionPeriod(endDateTimestamp: Timestamp): boolean {
+
+  isInSubscriptionPeriod(endDateTimestamp: Timestamp): boolean {
     if (endDateTimestamp) {
-      const now = new Date(); // Maybe it should get the current date from the server.
       const endDate = fromUnixTime(endDateTimestamp.seconds);
-      return now < endDate;
+      return this.today < endDate;
     }
     return false;
   }
+
   isPastEvent(endDateTimestamp: Timestamp): boolean {
     if (endDateTimestamp) {
-      const now = new Date(); // Maybe it should get the current date from the server.
       const endDate = fromUnixTime(endDateTimestamp.seconds);
-      return now > endDate;
+      return this.today > endDate;
     }
     return false;
   }
 
   getCardType(majorEvent: MajorEventItem, subscription: MajorEventSubscription): number {
-    const now = new Date();
     const eventEndDate = fromUnixTime(majorEvent.eventEndDate.seconds);
-    if (now > eventEndDate && subscription.payment.status === 2) return -1;
+    if (this.today > eventEndDate && subscription.payment.status === 2) return -1;
     return subscription.payment.status;
-  }
-
-  getSubscriptionSubtitle(subscriptionType: number, price: number): string {
-    const formattedPrice = price.toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-    });
-    switch (subscriptionType) {
-      case 0:
-        return `Aluno da Unesp - ${formattedPrice}`;
-      case 1:
-        return `Aluno de outra instituição - ${formattedPrice}`;
-      case 2:
-        return `Professor e/ou profissional - ${formattedPrice}`;
-      default:
-        return `Público em geral - ${formattedPrice}`;
-    }
   }
 }
 
