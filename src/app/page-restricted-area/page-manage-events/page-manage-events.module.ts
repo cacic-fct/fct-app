19db1b6627d720198de952aef79d0786e5b0dd9f
GuiCT <gtomiasi@gmail.com>
2022-10-01 15:07:23 -0300

    fixing page module

--- src/app/page-restricted-area/page-manage-events/page-manage-events.module.ts
+++ src/app/page-restricted-area/page-manage-events/page-manage-events.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { PageManageEventsRoutingModule } from './page-manage-events-routing.module';
-
+import { IonicModule } from '@ionic/angular';
+import { PageManageEvents } from './page-manage-events.page';
 
 @NgModule({
-  declarations: [],
   imports: [
     CommonModule,
-    PageManageEventsRoutingModule
-  ]
+    IonicModule,
+    PageManageEventsRoutingModule,
+  ],
+  declarations: [PageManageEvents],
 })
 export class PageManageEventsPageModule { }
