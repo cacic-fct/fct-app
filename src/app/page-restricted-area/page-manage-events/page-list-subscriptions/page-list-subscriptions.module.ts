02521632674418e76d6821770f3a0bdeb9c40150
GuiCT <gtomiasi@gmail.com>
2022-10-03 23:30:30 -0300

    feature: list subscriptions & generate CSV

--- src/app/page-restricted-area/page-manage-events/page-list-subscriptions/page-list-subscriptions.module.ts
+++ src/app/page-restricted-area/page-manage-events/page-list-subscriptions/page-list-subscriptions.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageListSubscriptionsRoutingModule } from './page-list-subscriptions-routing.module';
 import { PageListSubscriptions } from './page-list-subscriptions';
+import { IonicModule } from '@ionic/angular';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
   declarations: [PageListSubscriptions],
-  imports: [CommonModule, PageListSubscriptionsRoutingModule],
+  imports: [CommonModule, IonicModule, SweetAlert2Module, PageListSubscriptionsRoutingModule],
 })
 export class PageListSubscriptionsModule {}
