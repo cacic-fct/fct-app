9fce38ee595d9661c865cb6eebe65fcdc34996e7
Yudi <renanyaguinuma@hotmail.com>
2022-10-05 01:30:44 -0300

    Fix emoji regex

--- src/app/tab-calendar/components/item-list/item-list.component.ts
+++ src/app/tab-calendar/components/item-list/item-list.component.ts
@@ -65,7 +65,7 @@ export class ItemListComponent implements OnInit, OnChanges {
   }
 
   getEmoji(emoji: string): any {
-    if (emoji === undefined || !/^\p{Emoji}|\p{Emoji_Modifier}|$/u.test('emoji')) {
+    if (emoji === undefined || !/^\p{Emoji}|\p{Emoji_Modifier}$/u.test(emoji)) {
       // TODO: validar apenas 1 emoji
       return this.sanitizer.bypassSecurityTrustResourceUrl(parse('❔')[0].url);
     }
