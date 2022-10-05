7dadf4dd7e79c1b9506cf389f77d1eb867a8e713
Yudi <renanyaguinuma@hotmail.com>
2022-10-05 14:58:18 -0300

    wip

--- src/app/page-restricted-area/page-manage-major-events/validate-receipt/validate-receipt.page.ts
+++ src/app/page-restricted-area/page-manage-major-events/validate-receipt/validate-receipt.page.ts
@@ -3,7 +3,7 @@ import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/comp
 import { ActivatedRoute } from '@angular/router';
 import { Timestamp } from '@firebase/firestore-types';
 import { fromUnixTime } from 'date-fns';
-import { Observable, map, first } from 'rxjs';
+import { Observable, map, first, take } from 'rxjs';
 import { MajorEventItem } from 'src/app/shared/services/major-event.service';
 import { User } from 'src/app/shared/services/user';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
@@ -12,7 +12,7 @@ import { EventItem } from 'src/app/shared/services/event';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import * as firestore from 'firebase/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { AlertController, IonModal } from '@ionic/angular';
 
 @UntilDestroy()
@@ -58,9 +58,15 @@ export class ValidateReceiptPage implements OnInit {
       )
     );
     this.imgBaseHref = [this.eventId, 'payment-receipts'].join('/');
-    this.refuseForm = this.formBuilder.group({
-      errorMessage: ['', Validators.required],
-    });
+    this.refuseForm = this.formBuilder.group(
+      {
+        errorMessage: '',
+        radioGroup: ['', Validators.required],
+      },
+      {
+        validators: [this.validatorRadio],
+      }
+    );
   }
 
   imgURL(receiptId: string): string {
@@ -73,7 +79,7 @@ export class ValidateReceiptPage implements OnInit {
       .doc<User>(userId)
       .valueChanges()
       .pipe(
-        first(),
+        take(1),
         map((user) => user.displayName)
       );
   }
@@ -84,7 +90,7 @@ export class ValidateReceiptPage implements OnInit {
       .doc<EventItem>(eventId)
       .valueChanges()
       .pipe(
-        first(),
+        take(1),
         map((event) => ({
           name: event.name,
           availableSlots: event.slotsAvailable,
@@ -97,65 +103,105 @@ export class ValidateReceiptPage implements OnInit {
   }
 
   confirm() {
-    this.auth.user.pipe(first()).subscribe((user) => {
-      this.subscriptionsQuery.get().subscribe((col) => {
-        const docId = col.docs[0].id;
-        this.subscriptionsQuery.doc(docId).update({
-          // @ts-ignore
-          'payment.status': 2, // Novo status: pagamento aprovado
-          'payment.time': firestore.Timestamp.fromDate(new Date()), // Momento da mudança
-          'payment.author': user.uid, // Autor da mudança
+    this.auth.user.pipe(take(1)).subscribe((user) => {
+      this.subscriptionsQuery
+        .get()
+        .pipe(take(1), trace('firestore'))
+        .subscribe((col) => {
+          const docId = col.docs[0].id;
+          this.subscriptionsQuery.doc(docId).update({
+            // @ts-ignore
+            'payment.status': 2, // Novo status: pagamento aprovado
+            'payment.time': firestore.Timestamp.fromDate(new Date()), // Momento da mudança
+            'payment.author': user.uid, // Autor da mudança
+          });
+          this.swalConfirm.fire();
+          setTimeout(() => {
+            this.swalConfirm.close();
+          }, 1000);
         });
-        this.swalConfirm.fire();
-        setTimeout(() => {
-          this.swalConfirm.close();
-        }, 1000);
-      });
     });
   }
 
   refuse() {
     if (this.refuseForm.invalid) {
-      return; // Não permite recusa se faltar justificativa
+      return;
     }
-    this.auth.user.pipe(first()).subscribe((user) => {
-      this.subscriptionsQuery.get().subscribe((col) => {
-        const docId = col.docs[0].id;
-        this.subscriptionsQuery.doc(docId).update({
-          // @ts-ignore
-          'payment.status': 3, // Novo status: erro personalizado
-          'payment.time': firestore.Timestamp.fromDate(new Date()), // Momento da mudança
-          'payment.author': user.uid, // Autor da mudança
-          'payment.error': this.refuseForm.get('errorMessage').value, // Justificativa
-        });
-        // Fechando modal a partir do método dismiss()
-        this.refuseModal.dismiss();
 
-        this.afs
-          .doc(`majorEvents/${this.eventId}`)
-          .get()
-          .subscribe((doc) => {
-            const event = doc.data() as MajorEventItem;
-            const eventName = event.name;
+    this.auth.user.pipe(take(1)).subscribe((user) => {
+      this.subscriptionsQuery
+        .get()
+        .pipe(take(1), trace('firestore'))
+        .subscribe((col) => {
+          const docId = col.docs[0].id;
+
+          const docQuery = this.afs.doc(`users/${docId}`).get();
+
+          if (this.refuseForm.value.radioGroup === 'invalidReceipt') {
+            this.subscriptionsQuery.doc(docId).update({
+              // @ts-ignore
+              'payment.status': 3, // Novo status: erro personalizado
+              'payment.time': firestore.Timestamp.fromDate(new Date()),
+              'payment.author': user.uid,
+              'payment.error': this.refuseForm.get('errorMessage').value,
+            });
+
+            this.refuseModal.dismiss();
 
             this.afs
-              .doc(`users/${docId}`)
+              .doc(`majorEvents/${this.eventId}`)
               .get()
-              .subscribe((userDoc) => {
-                const user = userDoc.data() as User;
-                // Only first name from fullName
-                const firstName = user.fullName.split(' ')[0];
+              .pipe(take(1), trace('firestore'))
+              .subscribe((doc) => {
+                const event = doc.data() as MajorEventItem;
+                const eventName = event.name;
 
-                this.whatsAppAlert(firstName, user.phone, eventName, this.refuseForm.get('errorMessage').value);
+                docQuery.pipe(take(1), trace('firestore')).subscribe((userDoc) => {
+                  const user = userDoc.data() as User;
+                  // Only first name from fullName
+                  const firstName = user.fullName.split(' ')[0];
+
+                  this.whatsAppAlertInvalid(
+                    firstName,
+                    user.phone,
+                    eventName,
+                    this.refuseForm.get('errorMessage').value
+                  );
+                });
               });
-          });
-      });
+          } else if (this.refuseForm.value.radioGroup === 'noSlots') {
+            this.subscriptionsQuery.doc(docId).update({
+              // @ts-ignore
+              'payment.status': 4,
+              'payment.time': firestore.Timestamp.fromDate(new Date()),
+              'payment.author': user.uid,
+            });
+            this.refuseModal.dismiss();
+
+            this.afs
+              .doc(`majorEvents/${this.eventId}`)
+              .get()
+              .pipe(take(1), trace('firestore'))
+              .subscribe((doc) => {
+                const event = doc.data() as MajorEventItem;
+                const eventName = event.name;
+
+                docQuery.pipe(take(1), trace('firestore')).subscribe((userDoc) => {
+                  const user = userDoc.data() as User;
+                  // Only first name from fullName
+                  const firstName = user.fullName.split(' ')[0];
+
+                  this.whatsAppAlertNoSlots(firstName, user.phone, eventName);
+                });
+              });
+          }
+        });
     });
   }
 
-  async whatsAppAlert(name: string, phone: string, event: string, message: string) {
+  async whatsAppAlertInvalid(name: string, phone: string, event: string, message: string) {
     const alert = await this.alertController.create({
-      header: 'Enviar notificação por WhatsApp',
+      header: 'Enviar notificação por WhatsApp?',
       message: 'Envie uma mensagem para o usuário informando que o pagamento foi recusado.',
       buttons: [
         {
@@ -170,10 +216,7 @@ export class ValidateReceiptPage implements OnInit {
             let formattedPhone = phone.replace(/\D/g, '');
             formattedPhone = formattedPhone.replace(/^(\d{2})(\d)/g, '55$1$2');
 
-            const text: string = `Olá, ${name}! O seu comprovante de pagamento do evento "${event}" foi recusado.%0a
-            A justificativa é "${message}".%0a%0a
-            Realize o envio novamente pelo link:%0a
-            https://fct-pp.web.app/inscricoes/pagar/${this.eventId}?utm_source=whatsapp&utm_medium=message&utm_campaign=payment_error`;
+            const text: string = `Olá, ${name}! O seu comprovante de pagamento do evento "${event}" foi recusado.%0aA justificativa é "${message}".%0a%0aRealize o envio novamente pelo link:%0ahttps://fct-pp.web.app/inscricoes/pagar/${this.eventId}?utm_source=whatsapp&utm_medium=message&utm_campaign=payment_error`;
 
             const url = `https://api.whatsapp.com/send?phone=${formattedPhone}&text=${text}`;
             window.open(url, '_blank');
@@ -183,6 +226,45 @@ export class ValidateReceiptPage implements OnInit {
     });
 
     await alert.present();
+  }
+
+  async whatsAppAlertNoSlots(name: string, phone: string, event: string) {
+    const alert = await this.alertController.create({
+      header: 'Enviar notificação por WhatsApp?',
+      message: 'Envie uma mensagem para o usuário informando que não há mais vagas em um evento selecionado.',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel',
+        },
+        {
+          text: 'Sim',
+          role: 'confirm',
+          handler: () => {
+            // Format phone from 11 99999-9999 to 5511999999999
+            let formattedPhone = phone.replace(/\D/g, '');
+            formattedPhone = formattedPhone.replace(/^(\d{2})(\d)/g, '55$1$2');
+
+            const text: string = `Olá, ${name}! Ocorreu um problema com a sua inscrição no evento "${event}".%0aNão há mais vagas em uma das atividades selecionadas.%0a%0aVocê precisa editar a sua inscrição pelo link:%0ahttps://fct-pp.web.app/eventos/inscrever/${this.eventId}?utm_source=whatsapp&utm_medium=message&utm_campaign=no_slots`;
+            const url = `https://api.whatsapp.com/send?phone=${formattedPhone}&text=${text}`;
+            window.open(url, '_blank');
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  validatorRadio(control: AbstractControl): ValidationErrors | null {
+    if (control.get('radioGroup').value === 'invalidReceipt') {
+      control.get('errorMessage').addValidators(Validators.required);
+    } else {
+      control.get('errorMessage').removeValidators(Validators.required);
+    }
+
+    control.get('errorMessage').updateValueAndValidity({ onlySelf: true });
+    return null;
   }
 }
 
