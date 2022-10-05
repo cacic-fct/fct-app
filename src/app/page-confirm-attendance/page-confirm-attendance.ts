0f8e729242db8100ff659d78439b4c9c26af35b5
GuiCT <gtomiasi@gmail.com>
2022-10-05 18:18:03 -0300

    feature: write attendance to firestore
    escreve na coleção 'attendance' quando usuário pagou ou evento é não-pago
    escreve na coleção 'non-paying-attendance' quando usuário não pagou

--- src/app/page-confirm-attendance/page-confirm-attendance.ts
+++ src/app/page-confirm-attendance/page-confirm-attendance.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { trace } from '@angular/fire/compat/performance';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { map, Observable, take } from 'rxjs';
 import { EventItem } from '../shared/services/event';
-import { Timestamp } from '@firebase/firestore-types';
+import { Timestamp as TimestampType } from '@firebase/firestore-types';
+import { Timestamp } from '@firebase/firestore';
 import { fromUnixTime } from 'date-fns';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
+import { MajorEventItem } from '../shared/services/major-event.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 interface EventInfo {
   name: string;
-  attendanceCollectionEnd: Timestamp;
+  attendanceCollectionEnd: TimestampType;
 }
 
 @UntilDestroy()
@@ -25,6 +28,11 @@ export class PageConfirmAttendance implements OnInit {
   eventID: string;
   dataForm: FormGroup;
   eventInfo$: Observable<EventInfo>;
+  private eventRef: AngularFirestoreDocument<EventItem>;
+  private userID: string;
+  private isSubEvent: boolean;
+  private majorEventRef: AngularFirestoreDocument<MajorEventItem>;
+  private isPaid: boolean;
   private attendanceCode: string;
   @ViewChild('swalConfirm') private swalConfirm: SwalComponent;
 
@@ -32,7 +40,8 @@ export class PageConfirmAttendance implements OnInit {
     public formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private afs: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private auth: AngularFireAuth
   ) {}
 
   ngOnInit() {
@@ -40,10 +49,47 @@ export class PageConfirmAttendance implements OnInit {
     this.dataForm = this.formBuilder.group({
       code: ['', [Validators.required, this.codeValidator]],
     });
-    const eventValueChanges$: Observable<EventItem> = this.afs
-      .collection<EventItem>('events')
-      .doc(this.eventID)
-      .valueChanges();
+
+    this.auth.user.pipe(take(1), trace('auth')).subscribe((user) => {
+      this.userID = user.uid;
+    });
+
+    this.eventRef = this.afs.collection<EventItem>('events').doc(this.eventID);
+
+    const eventValueChanges$: Observable<EventItem> = this.eventRef.valueChanges();
+    // attendanceCode is constant
+    eventValueChanges$.pipe(take(1)).subscribe((event) => {
+      this.attendanceCode = event.attendanceCode;
+      if (event.inMajorEvent) {
+        // If it is a subEvent, then check to see if the majorEvent
+        // is paid or free.
+        this.isSubEvent = true;
+        // majorEvent reference
+        this.majorEventRef = this.afs.collection<MajorEventItem>('majorEvents').doc(event.inMajorEvent);
+        // Checking if majorEvent is paid
+        // This should be constant
+        this.majorEventRef
+          .valueChanges()
+          .pipe(take(1))
+          .subscribe((majorEvent) => {
+            if (majorEvent.price.isFree) {
+              this.isPaid = false;
+            } else {
+              this.isPaid = true;
+            }
+          });
+      } else {
+        this.isSubEvent = false;
+      }
+    });
+
+    // When the code is valid, automatically submit.
+    this.dataForm.get('code').valueChanges.subscribe((value) => {
+      if (value == this.attendanceCode) {
+        this.onSubmit();
+      }
+    });
+
     this.eventInfo$ = eventValueChanges$.pipe(
       trace('firestore'),
       untilDestroyed(this),
@@ -52,16 +98,6 @@ export class PageConfirmAttendance implements OnInit {
         attendanceCollectionEnd: event.attendanceCollectionEnd,
       }))
     );
-    // attendanceCode is constant
-    eventValueChanges$.pipe(take(1)).subscribe((event) => {
-      this.attendanceCode = event.attendanceCode;
-    });
-    // When the code is valid, automatically submit.
-    this.dataForm.get('code').valueChanges.subscribe((value) => {
-      if (value == this.attendanceCode) {
-        this.onSubmit();
-      }
-    });
   }
 
   codeValidator = (formControl: AbstractControl): { [key: string]: boolean } | null => {
@@ -75,11 +111,49 @@ export class PageConfirmAttendance implements OnInit {
     }
   };
 
-  getDateFromTimestamp(timestamp: Timestamp): Date {
+  getDateFromTimestamp(timestamp: TimestampType): Date {
     return fromUnixTime(timestamp.seconds);
   }
 
   onSubmit() {
+    const isPaymentNecessary: boolean = this.isSubEvent && this.isPaid;
+    if (isPaymentNecessary) {
+      // Check if user payment status = 2
+      this.majorEventRef
+        .collection('subscriptions')
+        .doc(this.userID)
+        .valueChanges()
+        .pipe(take(1), trace('firestore'))
+        .subscribe((subscriptionItem) => {
+          debugger;
+          if (subscriptionItem.payment.status == 2) {
+            // Escrevendo na coleção 'attendance'
+            this.eventRef
+              .collection('attendance')
+              .doc(this.userID)
+              .set({
+                time: Timestamp.fromDate(new Date()),
+              });
+          } else {
+            // Escrevendo na coleção 'non-paying-attendance'
+            this.eventRef
+              .collection('non-paying-attendance')
+              .doc(this.userID)
+              .set({
+                time: Timestamp.fromDate(new Date()),
+              });
+          }
+        });
+    } else {
+      // Escrevendo na coleção 'attendance'
+      this.eventRef
+        .collection('attendance')
+        .doc(this.userID)
+        .set({
+          time: Timestamp.fromDate(new Date()),
+        });
+    }
+
     this.swalConfirm.fire();
     setTimeout(() => {
       this.swalConfirm.close();
