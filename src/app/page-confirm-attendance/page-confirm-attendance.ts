eeef7ed70bbfbc437b793971bab4fd26f9e9135c
GuiCT <gtomiasi@gmail.com>
2022-10-05 00:56:16 -0300

    implementing basic code verifier

--- src/app/page-confirm-attendance/page-confirm-attendance.ts
+++ src/app/page-confirm-attendance/page-confirm-attendance.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { trace } from '@angular/fire/compat/performance';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Control } from 'ol/control';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { map, Observable, take } from 'rxjs';
+import { EventItem } from '../shared/services/event';
+import { Timestamp } from '@firebase/firestore-types';
+import { fromUnixTime } from 'date-fns';
 
+interface EventInfo {
+  name: string;
+  attendanceCollectionEnd: Timestamp;
+}
+
+@UntilDestroy()
 @Component({
   selector: 'app-page-confirm-attendance',
   templateUrl: './page-confirm-attendance.html',
@@ -12,6 +23,8 @@ import { Control } from 'ol/control';
 export class PageConfirmAttendance implements OnInit {
   eventID: string;
   dataForm: FormGroup;
+  eventInfo$: Observable<EventInfo>;
+  private attendanceCode: string;
 
   constructor(
     public formBuilder: FormBuilder,
@@ -22,15 +35,41 @@ export class PageConfirmAttendance implements OnInit {
 
   ngOnInit() {
     this.eventID = this.route.snapshot.params.eventID;
-    this.dataForm = this.formBuilder.group(
-      {
-        code: ['', [Validators.required, this.codeValidator]]
-      }
-    )
+    this.dataForm = this.formBuilder.group({
+      code: ['', [Validators.required, this.codeValidator]],
+    });
+    const eventValueChanges$: Observable<EventItem> = this.afs
+      .collection<EventItem>('events')
+      .doc(this.eventID)
+      .valueChanges();
+    this.eventInfo$ = eventValueChanges$.pipe(
+      trace('firestore'),
+      untilDestroyed(this),
+      map((event) => ({
+        name: event.name,
+        attendanceCollectionEnd: event.attendanceCollectionEnd,
+      }))
+    );
+    // attendanceCode is constant
+    eventValueChanges$.pipe(take(1)).subscribe((event) => {
+      this.attendanceCode = event.attendanceCode;
+    });
   }
 
-  codeValidator(formControl: AbstractControl): ValidationErrors | null {
-    return null;
+  codeValidator = (formControl: AbstractControl): { [key: string]: boolean } | null => {
+    debugger;
+    if (!this.attendanceCode) {
+      return { codeLoading: true };
+    }
+    if (formControl.value == this.attendanceCode) {
+      return null;
+    } else {
+      return { wrongCode: true };
+    }
+  };
+
+  getDateFromTimestamp(timestamp: Timestamp): Date {
+    return fromUnixTime(timestamp.seconds);
   }
 
   onSubmit() {}
