813f8303dbc4982236581d2cc8eb05bb77bfdde3
Yudi <renanyaguinuma@hotmail.com>
2022-10-11 10:04:42 -0300

    Add createEventSubscription, createMajorEventSubscription and updateMajorEventSubscription functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -195,14 +195,66 @@ exports.getUserUid = functions.https.onCall((data, context) => {
     });
 });
 
+exports.createEventSubscription = functions.firestore
+  .document(`events/{eventId}/subscriptions/{userId}`)
+  .onCreate(async (snap, context) => {
+    const eventRef = admin.firestore().collection('events').doc(context.params.eventId);
+    const event = await eventRef.get();
+    const numberOfSubscriptions = event.data()?.numberOfSubscriptions;
+    if (numberOfSubscriptions === undefined) {
+      return;
+    }
+    eventRef.update({ numberOfSubscriptions: admin.firestore.FieldValue.increment(1) });
+  });
 
-exports.createSubscription = functions.firestore.document(`events/{eventId}/subscriptions/{userId}`).onCreate(async (snap, context) => {
-  const eventId: string = context.params.eventId;
-  const eventSnap = await admin.firestore().collection('events').doc(eventId).get();
-  let numberOfSubscriptions: number =  eventSnap.data()?.numberOfSubscriptions;
-  numberOfSubscriptions = numberOfSubscriptions ? numberOfSubscriptions + 1 : 1;
-  await eventSnap.ref.update({ numberOfSubscriptions });
-});
+exports.createMajorEventSubscription = functions.firestore
+  .document(`majorEvents/{eventId}/subscriptions/{userId}`)
+  .onCreate(async (snap, context) => {
+    const data = snap.data();
+
+    // For each array in subscribedToEvents, increment the numberOfSubscriptions
+    data.subscribedToEvents.forEach((event: string) => {
+      const document = admin.firestore().doc(`events/${event}`);
+      document.update({
+        numberOfSubscriptions: admin.firestore.FieldValue.increment(1),
+      });
+    });
+  });
+
+exports.updateMajorEventSubscription = functions.firestore
+  .document(`majorEvents/{eventId}/subscriptions/{userId}`)
+  .onUpdate(async (change, context) => {
+    const beforeChange = change.before.data();
+    const afterChange = change.after.data();
+
+    if (beforeChange.subscribedToEvents !== afterChange.subscribedToEvents) {
+      // previouslySelectedEvents - eventsSelectedID = events that were removed
+      const removedEvents = beforeChange.subscribedToEvents.filter(
+        (event: string) => !afterChange.subscribedToEvents.includes(event)
+      );
+
+      // eventsSelectedID - previouslySelectedEvents = events that were added
+      const addedEvents = afterChange.subscribedToEvents.filter(
+        (event: string) => !beforeChange.subscribedToEvents.includes(event)
+      );
+
+      // For every removed event, decrement the number of subscribed users
+      removedEvents.forEach((event: string) => {
+        const document = admin.firestore().doc(`events/${event}`);
+        document.update({
+          numberOfSubscriptions: admin.firestore.FieldValue.increment(-1),
+        });
+      });
+
+      // For every added event, increment the number of subscribed users
+      addedEvents.forEach((event: string) => {
+        const document = admin.firestore().doc(`events/${event}`);
+        document.update({
+          numberOfSubscriptions: admin.firestore.FieldValue.increment(1),
+        });
+      });
+    }
+  });
 
 exports.addProfessorRole = functions.https.onCall((data, context) => {
   if (context.app == undefined) {
