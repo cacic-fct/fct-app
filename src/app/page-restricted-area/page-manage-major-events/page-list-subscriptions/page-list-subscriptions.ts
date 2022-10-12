511d5818cb90bc858f78cef99d594478b263aa28
Yudi <renanyaguinuma@hotmail.com>
2022-10-12 11:21:42 -0300

    Include more data in majorEvent subscriptions CSV export

--- src/app/page-restricted-area/page-manage-major-events/page-list-subscriptions/page-list-subscriptions.ts
+++ src/app/page-restricted-area/page-manage-major-events/page-list-subscriptions/page-list-subscriptions.ts
@@ -6,12 +6,13 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { SwalComponent } from '@sweetalert2/ngx-sweetalert2';
 import { fromUnixTime } from 'date-fns';
 import { Timestamp } from '@firebase/firestore-types';
-import { first, map, Observable } from 'rxjs';
+import { map, Observable, take, combineLatest } from 'rxjs';
 import { EventItem } from 'src/app/shared/services/event';
 import { User } from 'src/app/shared/services/user';
 import { CoursesService } from 'src/app/shared/services/courses.service';
+import { MajorEventSubscription } from './../../../shared/services/major-event.service';
 
-interface Subscription {
+interface Subscription extends MajorEventSubscription {
   id: string;
   time: Timestamp;
   user: Observable<User>;
@@ -60,7 +61,7 @@ export class PageListSubscriptions implements OnInit {
       .valueChanges()
       .pipe(trace('firestore'));
     this.subscriptions$ = this.afs
-      .collection<Subscription>(`majorEvents/${this.eventID}/subscriptions`, (ref) => ref.orderBy('time'))
+      .collection<MajorEventSubscription>(`majorEvents/${this.eventID}/subscriptions`, (ref) => ref.orderBy('time'))
       .valueChanges({ idField: 'id' })
       .pipe(
         untilDestroyed(this),
@@ -86,40 +87,119 @@ export class PageListSubscriptions implements OnInit {
     this.afs
       .collection<User>('users')
       .valueChanges({ idField: 'id' })
-      .pipe(first(), trace('firestore'))
+      .pipe(take(1), trace('firestore'))
       .subscribe((users) => {
         const csv = [];
-        const headers = ['UID', 'Nome da conta Google', 'Nome', 'RA', 'Email', 'Data_locale', 'Data_iso'];
+        const headers = [
+          'UID',
+          'Nome da conta Google',
+          'Nome completo',
+          'Vínculo com a Unesp',
+          'RA',
+          'Email',
+          'Código do status do pagamento',
+          'Status do pagamento',
+          'Data da última alteração no pagamento locale',
+          'Data da última alteração no pagamento ISO',
+          'Data da inscrição locale',
+          'Data da inscrição ISO',
+          'Inscreveu-se nos eventos com ID',
+          'Inscreveu-se nos eventos com nome',
+        ];
         csv.push(headers);
-        this.subscriptions$.pipe(first()).subscribe((subscriptions) => {
+        this.subscriptions$.pipe(take(1)).subscribe((subscriptions) => {
           subscriptions.forEach((item) => {
             const user = users.find((user) => user.uid === item.id);
-            const row = [
-              user.uid,
-              user.displayName,
-              user.fullName,
-              user.academicID,
-              user.email,
-              this.getDateFromTimestamp(item.time).toLocaleString('pt-BR', {
-                timeZone: 'America/Sao_Paulo',
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
-              }),
-              this.getDateFromTimestamp(item.time).toISOString(),
-            ];
-            csv.push(row);
-          });
 
-          this.event$.pipe(first()).subscribe((event) => {
-            const csvString = csv.map((row) => row.join(',')).join('\n');
-            const a = document.createElement('a');
-            a.href = window.URL.createObjectURL(new Blob([csvString], { type: 'text/csv' }));
-            a.download = `${event.name}_${this.getDateFromTimestamp(event.eventStartDate).toISOString()}.csv`;
-            a.click();
+            let status = 'Status não cadastrado';
+
+            switch (item.payment.status) {
+              case 0:
+                status = 'Aguardando envio do comprovante';
+                break;
+              case 1:
+                status = 'Comprovante em análise';
+                break;
+              case 2:
+                status = 'Pago e validado';
+                break;
+              case 3:
+                status = 'Devolvido por erro no comprovante';
+                break;
+              case 4:
+                status = 'Devolvido por falta de vagas';
+                break;
+            }
+
+            let subscribedToEventsItemArray$: Observable<EventItem>[] = [];
+
+            item.subscribedToEvents.forEach((eventID) => {
+              subscribedToEventsItemArray$.push(
+                this.afs
+                  .collection('events')
+                  .doc<EventItem>(eventID)
+                  .get()
+                  .pipe(map((doc) => doc.data()))
+              );
+            });
+
+            let subscribedToEvents$: Observable<EventItem[]> = combineLatest(subscribedToEventsItemArray$);
+
+            subscribedToEvents$.subscribe((events) => {
+              let eventNames = '';
+
+              events.forEach((event) => {
+                eventNames += '"' + event.name + '"; ';
+              });
+
+              const row = [
+                user.uid,
+                user.displayName,
+                user.fullName,
+                user.associateStatus,
+                user.academicID || 'Sem RA cadastrado',
+                user.email,
+                item.payment.status,
+                status,
+
+                this.getDateFromTimestamp(item.payment.time).toLocaleString('pt-BR', {
+                  timeZone: 'America/Sao_Paulo',
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                }),
+
+                this.getDateFromTimestamp(item.payment.time).toISOString(),
+
+                this.getDateFromTimestamp(item.time).toLocaleString('pt-BR', {
+                  timeZone: 'America/Sao_Paulo',
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                }),
+
+                this.getDateFromTimestamp(item.time).toISOString(),
+
+                item.subscribedToEvents.join('; '),
+
+                eventNames,
+              ];
+              csv.push(row);
+
+              this.event$.pipe(take(1)).subscribe((event) => {
+                const csvString = csv.map((row) => row.join(',')).join('\n');
+                const a = document.createElement('a');
+                a.href = window.URL.createObjectURL(new Blob([csvString], { type: 'text/csv' }));
+                a.download = `${event.name}_${this.getDateFromTimestamp(event.eventStartDate).toISOString()}.csv`;
+                a.click();
+              });
+            });
           });
         });
       });
