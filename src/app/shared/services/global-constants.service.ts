cb7f88b465fcfe71fe5455fa859d550f3657a299
Yudi <renanyaguinuma@hotmail.com>
2022-10-12 11:40:06 -0300

    Update version

--- src/app/shared/services/global-constants.service.ts
+++ src/app/shared/services/global-constants.service.ts
@@ -1,6 +1,6 @@
 export class GlobalConstantsService {
   public static appName: string = 'FCT App';
   /* YYYY.MM.DD.HH.MM */
-  public static appVersion: string = '2022.10.10.21.08.1';
+  public static appVersion: string = '2022.10.12.11.39';
   public static userDataVersion: string = '0.3.0';
 }
