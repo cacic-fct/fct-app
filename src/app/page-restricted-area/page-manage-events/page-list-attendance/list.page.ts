6503b667d84d8d93e42292ee424fb91242fdc813
Yudi <renanyaguinuma@hotmail.com>
2022-10-19 15:43:12 -0300

    Delete attendance button

--- src/app/page-restricted-area/page-manage-events/page-list-attendance/list.page.ts
+++ src/app/page-restricted-area/page-manage-events/page-list-attendance/list.page.ts
@@ -1,3 +1,4 @@
+import { ToastController, AlertController } from '@ionic/angular';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
@@ -35,7 +36,9 @@ export class ListPage implements OnInit {
     private afs: AngularFirestore,
     private router: Router,
     private route: ActivatedRoute,
-    public courses: CoursesService
+    public courses: CoursesService,
+    private toastController: ToastController,
+    private alertController: AlertController
   ) {}
 
   ngOnInit() {
@@ -78,6 +81,54 @@ export class ListPage implements OnInit {
 
   getDateFromTimestamp(timestamp: Timestamp): Date {
     return fromUnixTime(timestamp.seconds);
+  }
+
+  deleteAttendance(attendanceID: string) {
+    this.afs
+      .collection(`events/${this.eventID}/attendance`)
+      .doc(attendanceID)
+      .delete()
+      .then(() => {
+        this.deletedToast();
+      });
+  }
+
+  async deleteAlert(attendanceID: string, username: string) {
+    const alert = await this.alertController.create({
+      header: 'Tem certeza que deseja remover essa presença?',
+      message: username,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Sim',
+          role: 'confirm',
+          handler: () => {
+            this.deleteAttendance(attendanceID);
+          },
+        },
+      ],
+    });
+
+    alert.present();
+  }
+
+  async deletedToast() {
+    const toast = await this.toastController.create({
+      message: 'Presença removida',
+      duration: 2000,
+      icon: 'checkmark-circle-outline',
+      buttons: [
+        {
+          side: 'end',
+          text: 'OK',
+          role: 'cancel',
+        },
+      ],
+    });
+    toast.present();
   }
 
   generateCSV() {
