7aee198647b912239e094657ece4b541b1ccf935
Yudi <renanyaguinuma@hotmail.com>
2022-12-31 15:30:16 -0300

    Add missing paidMajorEvent_event2

--- functions/src/development-tools/populate-db/create-events/create-events.ts
+++ functions/src/development-tools/populate-db/create-events/create-events.ts
@@ -1,7 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { paidMajorEvent } from './major-event-data';
-import { event_data, group_event, paidMajorEvent_event1, paidMajorEvent_group_event } from './event-data';
+import {
+  event_data,
+  group_event,
+  paidMajorEvent_event1,
+  paidMajorEvent_event2,
+  paidMajorEvent_group_event,
+} from './event-data';
 
 exports.createPaidMajorEvent = functions.https.onCall(() => {
   const firestore = admin.firestore();
@@ -9,6 +15,7 @@ exports.createPaidMajorEvent = functions.https.onCall(() => {
   firestore.collection('majorEvents').doc('paidMajorEvent').set(paidMajorEvent, { merge: true });
 
   firestore.collection('events').doc('paidMajorEvent-event1').set(paidMajorEvent_event1, { merge: true });
+  firestore.collection('events').doc('paidMajorEvent-event2').set(paidMajorEvent_event2, { merge: true });
 
   for (let i = 1; i <= 2; i++) {
     firestore.collection('events').doc(`paidMajorEvent-event${i}`).set(paidMajorEvent_group_event(i), { merge: true });
