a36b894e5ae6aed5ca0922aa81e7ed6a39d3758a
Yudi <renanyaguinuma@hotmail.com>
2023-01-13 13:54:46 -0300

    Revert "Add ngx-extended-pdf-viewer"
    This reverts commit 531c8df788ba2a880f19cb691329fb80bee79f98.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -59,7 +59,6 @@ import {
   USE_DEVICE_LANGUAGE,
   USE_EMULATOR as USE_AUTH_EMULATOR,
 } from '@angular/fire/compat/auth';
-import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @NgModule({
   declarations: [AppComponent],
@@ -68,7 +67,6 @@ import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    NgxExtendedPdfViewerModule,
     AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAnalyticsModule,
