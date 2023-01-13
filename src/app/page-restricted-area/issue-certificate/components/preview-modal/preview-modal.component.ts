20a72b900710ac5f5f69bf980c6de54ecb7df15b
Yudi <renanyaguinuma@hotmail.com>
2023-01-13 12:23:49 -0300

    Don't use @pdfme/ui as it imports React

--- src/app/page-restricted-area/issue-certificate/components/preview-modal/preview-modal.component.ts
+++ src/app/page-restricted-area/issue-certificate/components/preview-modal/preview-modal.component.ts
@@ -4,8 +4,6 @@ import { take } from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
 
-import { Template, Viewer } from '@pdfme/ui';
-
 @Component({
   selector: 'app-preview-modal',
   templateUrl: './preview-modal.component.html',
@@ -14,36 +12,5 @@ import { Template, Viewer } from '@pdfme/ui';
 export class PreviewModalComponent implements OnInit {
   constructor(private certificateService: CertificateService, private http: HttpClient) {}
 
-  ngOnInit() {
-    this.previewCertificate();
-  }
-
-  previewCertificate() {
-    const domContainer = document.getElementById('pdf-viewer');
-    if (!domContainer) {
-      return;
-    }
-    const templateURL = this.certificateService.getTemplateURL('cacic');
-
-    this.http
-      .get(templateURL)
-      .pipe(take(1))
-      .subscribe((template: any) => {
-        if (template) {
-          const inputsa: CacicTemplateInput = {
-            date: 'Lorem',
-            participationType: 'Lorem',
-            eventType: 'Lorem',
-            content: 'Lorem',
-            name: 'Lorem',
-            eventName: 'Lorem',
-            document: 'Lorem',
-          };
-
-          const inputs = this.certificateService.cacicCertificatePreviewInputs(inputsa);
-
-          const viewer = new Viewer({ domContainer, template, inputs });
-        }
-      });
-  }
+  ngOnInit() {}
 }
