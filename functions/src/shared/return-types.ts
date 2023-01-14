04a2063d7e845a6659ecb37a22f08b07e27e1a76
Yudi <renanyaguinuma@hotmail.com>
2023-01-14 13:31:02 -0300

    Standardize return, convert all functions to async

---
+++ functions/src/shared/return-types.ts
@@ -0,0 +1,5 @@
+export interface MainReturnType {
+  message: string;
+  success: boolean;
+  data?: any;
+}
--- functions/src/utils/user/user-utils.ts
+++ functions/src/utils/user/user-utils.ts
@@ -1,10 +1,11 @@
 import * as functions from 'firebase-functions';
 import { getAuth } from 'firebase-admin/auth';
+import { MainReturnType } from '../../shared/return-types';
 
 // Attribution: The Net Ninja
 // https://youtube.com/watch?v=VvcBqPua2DI&list=PL4cUxeGkcC9jUPIes_B8vRjn1_GaplOPQ
 
-exports.getUserUid = functions.https.onCall((data, context) => {
+exports.getUserUid = functions.https.onCall(async (data, context): Promise<MainReturnType> => {
   if (!context.auth) {
     throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
@@ -21,20 +22,20 @@ exports.getUserUid = functions.https.onCall((data, context) => {
   }
 
   if (data.string === '') {
-    return { message: 'Invalid argument: A string must be provided.' };
+    return { message: 'Invalid argument: A string must be provided.', success: false };
   }
 
   if (data.string.includes('@')) {
-    return getAuth()
-      .getUserByEmail(data.string)
-      .then((user) => {
-        return {
-          uid: user.uid,
-        };
-      })
-      .catch((error) => {
-        return { message: `${error}` };
-      });
+    try {
+      const user = await getAuth().getUserByEmail(data.string);
+      return {
+        data: user.uid,
+        success: true,
+        message: 'User found by email',
+      };
+    } catch (error) {
+      return { message: `${error}`, success: false };
+    }
   }
   // Remove spaces from the string
   data.string = data.string.replace(/\s/g, '');
@@ -44,11 +45,11 @@ exports.getUserUid = functions.https.onCall((data, context) => {
 
   // Check if string only has numbers
   if (!isNumeric) {
-    return { message: 'Invalid argument: Invalid input' };
+    return { message: 'Invalid argument: Invalid input', success: false };
   }
 
   if (data.string.length < 11 || data.string.length > 14) {
-    return { message: 'Invalid argument: Invalid input' };
+    return { message: 'Invalid argument: Invalid input', success: false };
   }
 
   if (data.string.length === 11) {
@@ -57,14 +58,14 @@ exports.getUserUid = functions.https.onCall((data, context) => {
     data.string = `+${data.string}`;
   }
 
-  return getAuth()
-    .getUserByPhoneNumber(data.string)
-    .then((user) => {
-      return {
-        uid: user.uid,
-      };
-    })
-    .catch((error) => {
-      return { message: `${error}` };
-    });
+  try {
+    const user = await getAuth().getUserByPhoneNumber(data.string);
+    return {
+      data: user.uid,
+      success: true,
+      message: 'User found by phone number',
+    };
+  } catch (error) {
+    return { message: `${error}`, success: false };
+  }
 });
