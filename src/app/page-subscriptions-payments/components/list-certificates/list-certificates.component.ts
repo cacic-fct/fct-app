0feabd30b0de44b06d83c3f092d92ed7c15d0bfa
Yudi <renanyaguinuma@hotmail.com>
2023-01-15 13:34:23 -0300

    wip

--- src/app/page-subscriptions-payments/components/list-certificates/list-certificates.component.ts
+++ src/app/page-subscriptions-payments/components/list-certificates/list-certificates.component.ts
@@ -6,13 +6,9 @@ import { isDefined } from 'src/app/shared/services/rxjs.service';
 
 import { User } from '@firebase/auth';
 
-import {
-  GeneratePdfCertificateService,
-  generateCertificateOptions,
-} from './../../../shared/services/generate-pdf-certificate.service';
 import { first, map, Observable, take, switchMap, combineLatest } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { UserCertificateDocument } from 'src/app/shared/services/certificates.service';
+import { UserCertificateDocument, CertificateService } from 'src/app/shared/services/certificates.service';
 
 @Component({
   selector: 'app-list-certificates',
@@ -25,14 +21,14 @@ export class ListCertificatesComponent implements OnInit {
   certificatesColletion$: Observable<UserCertificateDocument[]>;
   userID!: string;
 
-  certificates$: Observable<any>;
+  certificates$!: Observable<any>;
 
   constructor(
-    private genpdf: GeneratePdfCertificateService,
     private modalController: ModalController,
     private mailtoService: MailtoService,
     private auth: AngularFireAuth,
-    private afs: AngularFirestore
+    private afs: AngularFirestore,
+    private certificateService: CertificateService
   ) {
     this.userData = JSON.parse(localStorage.getItem('user') as string);
 
@@ -49,23 +45,23 @@ export class ListCertificatesComponent implements OnInit {
 
     // Get all certificates based on this.certificatesColletion$ documment ids
 
-    this.certificates$ = this.certificatesColletion$.pipe(
-      map((certificates) => {
-        return certificates.map((certificate) => {
-          const options = {
-            name: this.userData.displayName,
-            event_name: certificate.eventName,
+    // this.certificates$ = this.certificatesColletion$.pipe(
+    //   map((certificates) => {
+    //     return certificates.map((certificate) => {
+    //       const options = {
+    //         name: this.userData.displayName,
+    //         event_name: certificate.eventName,
 
-            date: certificate.eventDate,
-            event_name_small: certificate.eventName,
-            name_small: this.userData.displayName,
-            certificateID: certificate.id,
-            userID: this.userID,
-          };
-          return options;
-        });
-      })
-    );
+    //         date: certificate.eventDate,
+    //         event_name_small: certificate.eventName,
+    //         name_small: this.userData.displayName,
+    //         certificateID: certificate.id,
+    //         userID: this.userID,
+    //       };
+    //       return options;
+    //     });
+    //   })
+    // );
 
     // this.certificates$ = this.certificatesColletion$.pipe(
     //   map(([certificates, user]) => {
@@ -102,103 +98,15 @@ export class ListCertificatesComponent implements OnInit {
   }
 
   async getCertificate() {
-    const inputs = {
-      name: 'Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano Francisco Xavier de Paula Leocádio Miguel',
-      event_name: 'SECOMPP22',
-      date: '13 de dezembro de 2022',
-      event_name_small: 'SECOMPP22',
-      name_small: 'Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano Francisco Xavier de Paula Leocádio Miguel',
-      content:
-        'Palestras:\n• 14/12/2022 10:45 - SECOMPP22 - Carga horária: 5 horas\n• 15/12/2022 10:45 - SECOMPP23 - Carga horária: 10 horas\nTotal: 15 horas - 2 palestras\n\nMinicursos:\n• 16/12/2022 10:45 - SECOMPP24 - Carga horária: 5 horas\n• 17/12/2022 10:45 - SECOMPP25 - Carga horária: 10 horas\nTotal: 15 horas - 2 minicursos\n\nAtividades:\n• 18/12/2022 10:45 - SECOMPP26 - Carga horária: 5 horas\nTotal: 5 horas - 1 atividade',
-      document: 'CPF: 000.000.000-00',
-    };
-
-    const options: generateCertificateOptions = {
-      eventType: 'majorEvent',
-      certificateID: 'certificateID',
-      certificateName: 'certificateName',
-    };
-    this.genpdf.generateCertificate('cacic', inputs, options);
+    this.certificateService.generateCertificate(
+      this.majorEventID,
+      {
+        certificateName: 'nombre',
+        certificateID: 've3n3KJqD1imLKJgeYTw',
+        eventType: 'algo',
+        participationType: 'algo',
+      },
+      'cacic'
+    );
   }
-}
-
-const generateContent = async (eventsUserAttended: string[], eventInfoCache: EventCacheObject) => {
-  const palestras: EventCache[] = [];
-  const minicursos: EventCache[] = [];
-  const uncategorized: EventCache[] = [];
-
-  // TODO: Considerar grupos de eventos
-  for (const eventID of eventsUserAttended) {
-    switch (eventInfoCache[eventID].eventType) {
-      case 'palestra':
-        palestras.push(eventInfoCache[eventID]);
-        break;
-      case 'minicurso':
-        minicursos.push(eventInfoCache[eventID]);
-        break;
-      default:
-        uncategorized.push(eventInfoCache[eventID]);
-        break;
-    }
-  }
-
-  // Sort events by date
-  palestras.sort((a, b) => a.eventStartDate.toMillis() - b.eventStartDate.toMillis());
-  minicursos.sort((a, b) => a.eventStartDate.toMillis() - b.eventStartDate.toMillis());
-  uncategorized.sort((a, b) => a.eventStartDate.toMillis() - b.eventStartDate.toMillis());
-
-  // Generate content string
-  let content = '';
-
-  content += makeText('Palestra', palestras);
-  content += makeText('Minicurso', minicursos);
-  content += makeText('Atividade', uncategorized);
-
-  console.log(content);
-
-  return content;
-};
-
-function makeText(type: string, array: EventCache[]): string {
-  let content = type;
-
-  if (array.length === 0) {
-    return '';
-  }
-
-  if (array.length > 1) {
-    // If last character is 'm' replace it with 'ns'
-    if (content.slice(-1) === 'm') {
-      content = content.slice(0, -1) + 'ns';
-    } else if (content.slice(-2) === 'ão') {
-      content = content.slice(0, -2) + 'ões';
-    } else {
-      content += 's';
-    }
-  }
-  content += ':\n';
-
-  let totalWorkload = 0;
-  for (const event of array) {
-    content += `• ${formatTimestamp(event.eventStartDate)} - ${event.eventName} - Carga horária: ${
-      event.workload ? event.workload + ' horas' : 'indefinida'
-    };\n`;
-  }
-  content = content.slice(0, -2) + '.\n';
-  content += `\nCarga horária total: ${totalWorkload} horas\n`;
-  content += '\n\n';
-
-  return content;
-}
-
-function convertTimezone(date: Date, timezone: string): Date {
-  const convertedDate = new Date(date.toLocaleString('en-US', { timeZone: timezone }));
-  return convertedDate;
-}
-
-function formatTimestamp(date: Timestamp): string {
-  let dateFormatted = date.toDate();
-  dateFormatted = convertTimezone(dateFormatted, 'America/Sao_Paulo');
-
-  return formatDate(dateFormatted, 'dd/MM/yyyy - HH:mm');
 }
--- src/app/shared/services/certificates.service.ts
+++ src/app/shared/services/certificates.service.ts
@@ -1,5 +1,212 @@
+import { EventItem } from 'src/app/shared/services/event';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { DocumentReference } from '@angular/fire/firestore';
 import { Timestamp } from '@firebase/firestore-types';
+import { Observable, take, switchMap, combineLatest, map } from 'rxjs';
+import { Injectable, isDevMode } from '@angular/core';
+import { format as formatDate } from 'date-fns';
+
+import { Template, generate as PDFGenerate } from '@pdfme/generator';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CertificateService {
+  constructor(private afs: AngularFirestore, private http: HttpClient) {}
+
+  generateCertificate(eventID: string, certificateData: any, certificateUserData: any) {
+    let pdfJson: Observable<Object>;
+    const pdfPath = `assets/certificates/templates/${certificateData.certificateTemplate}.json`;
+    if (isDevMode()) {
+      pdfJson = this.http.get(pdfPath, {
+        responseType: 'json',
+      });
+    } else {
+      pdfJson = this.http.get(`https://cdn.jsdelivr.net/gh/cacic-fct/fct-app@main/src/${pdfPath}`, {
+        responseType: 'json',
+      });
+    }
+
+    pdfJson.pipe(take(1)).subscribe(async (pdf) => {
+      const template = pdf as Template;
+
+      let font = {};
+
+      switch (certificateData.certificateTemplate) {
+        case 'cacic':
+          font = {
+            Inter_Regular: {
+              data: await fetch(
+                'https://cdn.jsdelivr.net/gh/cacic-fct/fonts@main/Inter/latin-ext/inter-v12-latin-ext-regular.woff'
+              ).then((res) => res.arrayBuffer()),
+              fallback: true,
+            },
+            Inter_Medium: {
+              data: await fetch(
+                'https://cdn.jsdelivr.net/gh/cacic-fct/fonts@main/Inter/latin-ext/inter-v12-latin-ext-500.woff'
+              ).then((res) => res.arrayBuffer()),
+            },
+            Inter_Light: {
+              data: await fetch(
+                'https://cdn.jsdelivr.net/gh/cacic-fct/fonts@main/Inter/latin-ext/inter-v12-latin-ext-300.woff'
+              ).then((res) => res.arrayBuffer()),
+            },
+          };
+          break;
+      }
+
+      const verificationURL = `https://fct-pp.web.app/certificado/verificar/${eventID}-${certificateUserData.id}`;
+
+      this.getCertificateContent(eventID, certificateData.certificateID)
+        .pipe(take(1))
+        .subscribe((content) => {
+          console.log(content);
+          let input = {
+            name: certificateUserData.fullName,
+            document: certificateUserData.document,
+            event_type: certificateData.eventType,
+            participation_type: certificateData.participationType,
+            url: verificationURL,
+            qrcode: verificationURL,
+            qrcode2: verificationURL,
+            content: content,
+          };
+
+          const inputs = [input];
+
+          PDFGenerate({ template, inputs, options: { font } }).then((pdf) => {
+            const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
+            const pdfUrl = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = pdfUrl;
+            a.download = certificateData.certificateName + '.pdf';
+            a.click();
+          });
+        });
+    });
+  }
+
+  getCertificateContent(eventID: string, certificateID: string): Observable<string> {
+    // Get events user attended from /certificates/{eventID}/{certificateID}
+    const certificate$ = this.afs.doc<CertificateDocPublic>(`certificates/${eventID}/${certificateID}/public`).get();
+
+    return certificate$.pipe(
+      take(1),
+      switchMap((certificate) => {
+        if (certificate.exists) {
+          // For every element in attendedEvents, get event info from /events/{eventID}
+          const eventInfoCache: Observable<EventItem | undefined>[] = [];
+          const eventsUserAttended = certificate.data()?.attendedEvents;
+
+          if (eventsUserAttended === undefined) {
+            return '';
+          }
+
+          // Get event info from cache
+          for (const eventID of eventsUserAttended) {
+            eventInfoCache.push(this.afs.doc<EventItem>(`events/${eventID}`).valueChanges());
+          }
+
+          // Generate content
+          const a = generateContent(eventInfoCache);
+          return a;
+        } else {
+          return '';
+        }
+      })
+    );
+  }
+}
+function generateContent(eventInfoCache: Observable<EventItem | undefined>[]): Observable<string> {
+  const eventInfo$ = combineLatest(eventInfoCache);
+
+  return eventInfo$.pipe(
+    take(1),
+    map((eventInfo) => {
+      const minicursos = [];
+      const palestras = [];
+      const uncategorized = [];
+
+      for (const event of eventInfo) {
+        if (event === undefined) {
+          continue;
+        }
+        switch (event.eventType) {
+          case 'minicurso':
+            minicursos.push(event);
+            break;
+          case 'palestra':
+            palestras.push(event);
+            break;
+          default:
+            uncategorized.push(event);
+            break;
+        }
+      }
+
+      let content = '';
+      if (minicursos.length > 0) {
+        content += makeText('Minicurso', 'Minicursos', minicursos);
+      }
+
+      if (palestras.length > 0) {
+        content += makeText('Palestra', 'Palestras', palestras);
+      }
+
+      if (uncategorized.length > 0) {
+        content += makeText('Atividade', 'Atividades', uncategorized);
+      }
+
+      const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      const userTimezoneOffset = new Date().getTimezoneOffset() / 60;
+      const userTimezoneOffsetString = `${userTimezoneOffset > 0 ? '-' : '+'}${Math.abs(userTimezoneOffset)}`;
+
+      content += `\nDatas de acordo com o fuso horário ${userTimezone} (UTC${userTimezoneOffsetString}).`;
+
+      return content;
+    })
+  );
+}
+
+function makeText(typeSingular: string, typePlural: string, array: EventItem[]): string {
+  if (array.length === 0) {
+    return '';
+  }
+
+  let content = '';
+
+  if (array.length > 1) {
+    content = typePlural;
+  } else {
+    content = typeSingular;
+  }
+
+  content += ':\n';
+
+  let totalCreditHours = 0;
+  for (const event of array) {
+    content += `• ${formatTimestamp(event.eventStartDate)} - ${event.name} - Carga horária: ${
+      event.creditHours ? event.creditHours + ' horas' : 'indefinida'
+    };\n`;
+  }
+  content = content.slice(0, -2) + '.\n';
+  content += `Carga horária total: ${totalCreditHours} horas.\n\n`;
+
+  return content;
+}
+
+function convertTimezone(date: Date, timezone: string): Date {
+  const convertedDate = new Date(date.toLocaleString('en-US', { timeZone: timezone }));
+  return convertedDate;
+}
+
+function formatTimestamp(date: Timestamp): string {
+  let dateFormatted = date.toDate();
+  dateFormatted = convertTimezone(dateFormatted, 'America/Sao_Paulo');
+
+  return formatDate(dateFormatted, 'dd/MM/yyyy - HH:mm');
+}
 
 export interface Certificate {
   /**
@@ -101,7 +308,19 @@ interface CertificateIssuingData {
   };
 }
 
+interface CertificateDocPublic {
+  certificateID: string;
+  document: string;
+  fullName: string;
+  issueDate: Timestamp;
+  attendedEvents: string[];
+}
+
 export interface UserCertificateDocument {
   publicReference: DocumentReference;
   certificateName: string;
+}
+
+interface EventCache {
+  [eventID: string]: Observable<EventItem | undefined>;
 }
