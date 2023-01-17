51fe670dd4b8cae31a4b9536d078f1a6a91e105f
Yudi <renanyaguinuma@hotmail.com>
2023-01-17 11:53:41 -0300

    Update version

--- src/app/shared/services/global-constants.service.ts
+++ src/app/shared/services/global-constants.service.ts
@@ -1,7 +1,7 @@
 export class GlobalConstantsService {
   public static appName: string = 'FCT App';
   /* YYYY.MM.DD.HH.MM */
-  public static appVersion: string = '2023.01.16.17.42';
+  public static appVersion: string = '2023.01.17.11.53';
   public static userDataVersion: string = '0.3.0';
   public static nonAmbiguousAlphabetCapitalizedNumbers: string = '2345689ABCDEFGHKMNPQRSTWXYZ';
 }
