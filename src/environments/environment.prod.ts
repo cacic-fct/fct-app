c32fed3b409da0cce90ee7e52045fa03e5e807b7
Yudi <renanyaguinuma@hotmail.com>
2023-05-18 11:38:39 -0300

    Add appId to environment.firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
+    projectId: 'fct-pp',
+    appId: '1:169157391934:web:ba031cc3f7dcafc057c36f',
+    storageBucket: 'fct-pp.appspot.com',
+    locationId: 'southamerica-east1',
     apiKey: 'AIzaSyAGtd5nwgEC68dvzTtA1TvCt9LGZ9luWhk',
     authDomain: 'fct-pp.firebaseapp.com',
-    projectId: 'fct-pp',
-    storageBucket: 'fct-pp.appspot.com',
     messagingSenderId: '169157391934',
-    appId: '1:169157391934:web:ba031cc3f7dcafc057c36f',
     measurementId: 'G-BKYCC0GR6G',
   },
   recaptcha3SiteKey: '6LcFr9AdAAAAANB7MbCks-nBVqfZZYp8bGp6Kcto',
