debe97bceb0c84daa4ec8362abff4589727df247
Yudi <12040397+Yudi@users.noreply.github.com>
2024-02-24 23:52:34 -0300

    Update version

--- src/app/shared/services/global-constants.service.ts
+++ src/app/shared/services/global-constants.service.ts
@@ -1,7 +1,7 @@
 export class GlobalConstantsService {
   public static appName: string = 'FCT App';
   /* YYYY.MM.DD.HH.MM */
-  public static appVersion: string = '2023.12.24.23.32';
+  public static appVersion: string = '2024.02.24.23.52';
   public static userDataVersion: string = '0.3.0';
   public static nonAmbiguousAlphabetCapitalizedNumbers: string = '2345689ABCDEFGHKMNPQRSTWXYZ';
 }
