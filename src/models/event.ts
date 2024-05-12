8d55e60977c5bd4cb92d4650670d7b431e12dfe9
karol marques <karoldm1579@gmail.com>
2024-05-12 17:36:44 -0300

    refactor: about pages dir name and types

---
+++ src/models/event.ts
@@ -0,0 +1,15 @@
+interface Event {
+  initDate: Date
+  finishDate: Date
+  title: string
+  id: string
+}
+
+interface BigEvent {
+  initDate: Date
+  finishDate: Date
+  events: Event[]
+  id: string
+}
+
+export type { Event, BigEvent };
