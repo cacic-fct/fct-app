0a3b40b89245bcf165e7c0939c180c2fef4982dd
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-15 21:11:18 -0300

    Fix angular-fire providers

--- src/main.ts
+++ src/main.ts
@@ -68,78 +68,78 @@ bootstrapApplication(AppComponent, {
       AngularFirestoreModule, //.enablePersistence({ synchronizeTabs: true }),
 
       AngularFirePerformanceModule,
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideAppCheck(() => {
-        const provider = new ReCaptchaV3Provider(environment.recaptcha3SiteKey);
-        return initializeAppCheck(getApp(), {
-          provider,
-          isTokenAutoRefreshEnabled: true,
-        });
-      }),
-      provideAuth(() => {
-        const auth = getAuth();
-        useDeviceLanguage(auth);
-        if (environment.firebase.useEmulators) {
-          connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-        }
-        return auth;
-      }),
-      provideRemoteConfig(() => {
-        const remoteConfig = getRemoteConfig();
-        if (isDevMode()) {
-          remoteConfig.settings.minimumFetchIntervalMillis = 10000;
-          remoteConfig.settings.fetchTimeoutMillis = 60000;
-        } else {
-          remoteConfig.settings.minimumFetchIntervalMillis = 43200000;
-          remoteConfig.settings.fetchTimeoutMillis = 60000;
-        }
-        remoteConfig.defaultConfig = {
-          calendarItemViewDefault: true,
-          mapTabEnabled: true,
-          manualTabEnabled: false,
-          eventsTabEnabled: true,
-          registerPrompt: true,
-        };
-        fetchAndActivate(remoteConfig).catch((err) => {
-          console.error(err);
-        });
-        return remoteConfig;
-      }),
-      provideAnalytics(() => {
-        const analytics = getAnalytics();
-        logEvent(analytics, 'app_version', {
-          app_name: GlobalConstantsService.appName,
-        });
-        return analytics;
-      }),
-      provideStorage(() => {
-        const storage = getStorage();
-        if (environment.firebase.useEmulators) {
-          connectStorageEmulator(storage, 'localhost', 9199);
-        }
-        return storage;
-      }),
-      provideFunctions(() => {
-        const functions = getFunctions(getApp(), 'southamerica-east1');
-        if (environment.firebase.useEmulators) {
-          connectFunctionsEmulator(functions, 'localhost', 5001);
-        }
-        return functions;
-      }),
-      provideFirestore(() => {
-        const firestore = initializeFirestore(getApp(), {
-          ignoreUndefinedProperties: true,
-
-          // TODO: https://github.com/cacic-fct/fct-app/issues/172
-          // localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
-        });
-        if (environment.firebase.useEmulators) {
-          connectFirestoreEmulator(firestore, 'localhost', 8081);
-        }
-        return firestore;
-      }),
     ),
     PerformanceMonitoringService,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAppCheck(() => {
+      const provider = new ReCaptchaV3Provider(environment.recaptcha3SiteKey);
+      return initializeAppCheck(getApp(), {
+        provider,
+        isTokenAutoRefreshEnabled: true,
+      });
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      useDeviceLanguage(auth);
+      if (environment.firebase.useEmulators) {
+        connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+      }
+      return auth;
+    }),
+    provideRemoteConfig(() => {
+      const remoteConfig = getRemoteConfig();
+      if (isDevMode()) {
+        remoteConfig.settings.minimumFetchIntervalMillis = 10000;
+        remoteConfig.settings.fetchTimeoutMillis = 60000;
+      } else {
+        remoteConfig.settings.minimumFetchIntervalMillis = 43200000;
+        remoteConfig.settings.fetchTimeoutMillis = 60000;
+      }
+      remoteConfig.defaultConfig = {
+        calendarItemViewDefault: true,
+        mapTabEnabled: true,
+        manualTabEnabled: false,
+        eventsTabEnabled: true,
+        registerPrompt: true,
+      };
+      fetchAndActivate(remoteConfig).catch((err) => {
+        console.error(err);
+      });
+      return remoteConfig;
+    }),
+    provideAnalytics(() => {
+      const analytics = getAnalytics();
+      logEvent(analytics, 'app_version', {
+        app_name: GlobalConstantsService.appName,
+      });
+      return analytics;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      if (environment.firebase.useEmulators) {
+        connectStorageEmulator(storage, 'localhost', 9199);
+      }
+      return storage;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions(getApp(), 'southamerica-east1');
+      if (environment.firebase.useEmulators) {
+        connectFunctionsEmulator(functions, 'localhost', 5001);
+      }
+      return functions;
+    }),
+    provideFirestore(() => {
+      const firestore = initializeFirestore(getApp(), {
+        ignoreUndefinedProperties: true,
+
+        // TODO: https://github.com/cacic-fct/fct-app/issues/172
+        // localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
+      });
+      if (environment.firebase.useEmulators) {
+        connectFirestoreEmulator(firestore, 'localhost', 8081);
+      }
+      return firestore;
+    }),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
     { provide: FIRESTORE_SETTINGS, useValue: { ignoreUndefinedProperties: true, merge: true } },
     { provide: USE_FIRESTORE_EMULATOR, useValue: environment.firebase.useEmulators ? ['localhost', 8081] : undefined },
