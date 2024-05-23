6bffc71e64e3bf9ed6a167056781d600543d99ab
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-23 03:45:39 -0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -21,18 +21,18 @@ import { provideAppCheck, initializeAppCheck } from '@angular/fire/app-check';
 // TODO: This should be imported from '@angular/fire/app-check' but it's not available
 import { ReCaptchaV3Provider } from 'firebase/app-check';
 
-// import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireModule } from '@angular/fire/compat';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { MarkdownModule } from 'ngx-markdown';
 import { withInterceptorsFromDi, provideHttpClient, HttpClient } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { routes } from 'src/app/app.routes';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-// import {
-//   SETTINGS as FIRESTORE_SETTINGS,
-//   USE_EMULATOR as USE_FIRESTORE_EMULATOR,
-//   AngularFirestoreModule,
-// } from '@angular/fire/compat/firestore';
+import {
+  SETTINGS as FIRESTORE_SETTINGS,
+  USE_EMULATOR as USE_FIRESTORE_EMULATOR,
+  AngularFirestoreModule,
+} from '@angular/fire/compat/firestore';
 import { IonicRouteStrategy, isPlatform, provideIonicAngular } from '@ionic/angular/standalone';
 import { PreloadingStrategyService } from 'src/app/shared/services/routing/preloading-strategy.service';
 
@@ -63,10 +63,10 @@ bootstrapApplication(AppComponent, {
       MarkdownModule.forRoot({ loader: HttpClient }),
       SweetAlert2Module.forRoot(),
       // AngularFire
-      // AngularFireModule.initializeApp(environment.firebase),
+      AngularFireModule.initializeApp(environment.firebase),
 
       // TODO: https://github.com/cacic-fct/fct-app/issues/172
-      // AngularFirestoreModule, //.enablePersistence({ synchronizeTabs: true }),
+      AngularFirestoreModule, //.enablePersistence({ synchronizeTabs: true }),
     ),
 
     provideFirebaseApp(() => initializeApp(environment.firebase)),
@@ -140,8 +140,8 @@ bootstrapApplication(AppComponent, {
       return firestore;
     }),
     { provide: LOCALE_ID, useValue: 'pt-BR' },
-    // { provide: FIRESTORE_SETTINGS, useValue: { ignoreUndefinedProperties: true, merge: true } },
-    // { provide: USE_FIRESTORE_EMULATOR, useValue: environment.firebase.useEmulators ? ['localhost', 8081] : undefined },
+    { provide: FIRESTORE_SETTINGS, useValue: { ignoreUndefinedProperties: true, merge: true } },
+    { provide: USE_FIRESTORE_EMULATOR, useValue: environment.firebase.useEmulators ? ['localhost', 8081] : undefined },
     provideHttpClient(withInterceptorsFromDi()),
   ],
 }).catch((err) => console.log(err));
