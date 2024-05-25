e3e43d9b314c15291a1461f6045d0bb82d596f5a
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-24 23:30:00 -0300

    Try a different nonce apprach
    With cookie

--- src/main.ts
+++ src/main.ts
@@ -53,121 +53,120 @@ if (environment.production) {
   enableProdMode();
 }
 
-async function fetchNonce(): Promise<string> {
-  const response = await fetch('assets/nonce.txt', { cache: 'no-store' });
-  if (!response.ok) {
-    throw new Error('Failed to fetch nonce');
+function fetchNonce(): string {
+  const nonce = document.cookie.split(';').find((cookie) => cookie.startsWith('nonce='));
+  if (!nonce) {
+    throw new Error('Nonce not found in cookies');
   }
-  return await response.text();
+
+  return nonce.split('=')[1];
 }
 
-fetchNonce().then((nonce) => {
-  console.log('Fetched nonce:', nonce);
-  setNonce(nonce);
+const nonce = fetchNonce();
+setNonce(nonce);
 
-  bootstrapApplication(AppComponent, {
-    providers: [
-      provideExperimentalZonelessChangeDetection(),
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
 
-      { provide: CSP_NONCE, useValue: nonce },
-      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-      provideIonicAngular({
-        backButtonText: isPlatform('ios') ? 'Voltar' : undefined,
+    { provide: CSP_NONCE, useValue: nonce },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular({
+      backButtonText: isPlatform('ios') ? 'Voltar' : undefined,
+    }),
+    provideRouter(routes, withPreloading(PreloadingStrategyService), withComponentInputBinding()),
+    importProvidersFrom(
+      BrowserModule,
+      ServiceWorkerModule.register(unwrapResourceUrl(trustedResourceUrl`/ngsw-worker.js`) as string, {
+        enabled: environment.production,
+        registrationStrategy: 'registerImmediately',
       }),
-      provideRouter(routes, withPreloading(PreloadingStrategyService), withComponentInputBinding()),
-      importProvidersFrom(
-        BrowserModule,
-        ServiceWorkerModule.register(unwrapResourceUrl(trustedResourceUrl`/ngsw-worker.js`) as string, {
-          enabled: environment.production,
-          registrationStrategy: 'registerImmediately',
-        }),
-        // Other modules
-        MarkdownModule.forRoot({ loader: HttpClient }),
-        SweetAlert2Module.forRoot(),
-        // AngularFire
-        AngularFireModule.initializeApp(environment.firebase),
+      // Other modules
+      MarkdownModule.forRoot({ loader: HttpClient }),
+      SweetAlert2Module.forRoot(),
+      // AngularFire
+      AngularFireModule.initializeApp(environment.firebase),
+
+      // TODO: https://github.com/cacic-fct/fct-app/issues/172
+      AngularFirestoreModule, //.enablePersistence({ synchronizeTabs: true }),
+    ),
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAppCheck(() => {
+      const provider = new ReCaptchaV3Provider(environment.recaptcha3SiteKey);
+      return initializeAppCheck(getApp(), {
+        provider,
+        isTokenAutoRefreshEnabled: true,
+      });
+    }),
+    provideAuth(() => {
+      const auth = getAuth();
+      useDeviceLanguage(auth);
+      if (environment.firebase.useEmulators) {
+        connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
+      }
+      return auth;
+    }),
+    provideRemoteConfig(() => {
+      const remoteConfig = getRemoteConfig();
+      if (isDevMode()) {
+        remoteConfig.settings.minimumFetchIntervalMillis = 10000;
+        remoteConfig.settings.fetchTimeoutMillis = 60000;
+      } else {
+        remoteConfig.settings.minimumFetchIntervalMillis = 43200000;
+        remoteConfig.settings.fetchTimeoutMillis = 60000;
+      }
+      remoteConfig.defaultConfig = {
+        calendarItemViewDefault: true,
+        mapTabEnabled: true,
+        manualTabEnabled: false,
+        eventsTabEnabled: true,
+        registerPrompt: true,
+      };
+      fetchAndActivate(remoteConfig).catch((err) => {
+        console.error(err);
+      });
+      return remoteConfig;
+    }),
+    provideAnalytics(() => {
+      const analytics = getAnalytics();
+      logEvent(analytics, 'app_version', {
+        app_name: GlobalConstantsService.appName,
+      });
+      return analytics;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      if (environment.firebase.useEmulators) {
+        connectStorageEmulator(storage, 'localhost', 9199);
+      }
+      return storage;
+    }),
+    provideFunctions(() => {
+      const functions = getFunctions(getApp(), 'southamerica-east1');
+      if (environment.firebase.useEmulators) {
+        connectFunctionsEmulator(functions, 'localhost', 5001);
+      }
+      return functions;
+    }),
+    provideFirestore(() => {
+      const firestore = initializeFirestore(getApp(), {
+        ignoreUndefinedProperties: true,
 
         // TODO: https://github.com/cacic-fct/fct-app/issues/172
-        AngularFirestoreModule, //.enablePersistence({ synchronizeTabs: true }),
-      ),
-
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideAppCheck(() => {
-        const provider = new ReCaptchaV3Provider(environment.recaptcha3SiteKey);
-        return initializeAppCheck(getApp(), {
-          provider,
-          isTokenAutoRefreshEnabled: true,
-        });
-      }),
-      provideAuth(() => {
-        const auth = getAuth();
-        useDeviceLanguage(auth);
-        if (environment.firebase.useEmulators) {
-          connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-        }
-        return auth;
-      }),
-      provideRemoteConfig(() => {
-        const remoteConfig = getRemoteConfig();
-        if (isDevMode()) {
-          remoteConfig.settings.minimumFetchIntervalMillis = 10000;
-          remoteConfig.settings.fetchTimeoutMillis = 60000;
-        } else {
-          remoteConfig.settings.minimumFetchIntervalMillis = 43200000;
-          remoteConfig.settings.fetchTimeoutMillis = 60000;
-        }
-        remoteConfig.defaultConfig = {
-          calendarItemViewDefault: true,
-          mapTabEnabled: true,
-          manualTabEnabled: false,
-          eventsTabEnabled: true,
-          registerPrompt: true,
-        };
-        fetchAndActivate(remoteConfig).catch((err) => {
-          console.error(err);
-        });
-        return remoteConfig;
-      }),
-      provideAnalytics(() => {
-        const analytics = getAnalytics();
-        logEvent(analytics, 'app_version', {
-          app_name: GlobalConstantsService.appName,
-        });
-        return analytics;
-      }),
-      provideStorage(() => {
-        const storage = getStorage();
-        if (environment.firebase.useEmulators) {
-          connectStorageEmulator(storage, 'localhost', 9199);
-        }
-        return storage;
-      }),
-      provideFunctions(() => {
-        const functions = getFunctions(getApp(), 'southamerica-east1');
-        if (environment.firebase.useEmulators) {
-          connectFunctionsEmulator(functions, 'localhost', 5001);
-        }
-        return functions;
-      }),
-      provideFirestore(() => {
-        const firestore = initializeFirestore(getApp(), {
-          ignoreUndefinedProperties: true,
-
-          // TODO: https://github.com/cacic-fct/fct-app/issues/172
-          // localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
-        });
-        if (environment.firebase.useEmulators) {
-          connectFirestoreEmulator(firestore, 'localhost', 8081);
-        }
-        return firestore;
-      }),
-      { provide: LOCALE_ID, useValue: 'pt-BR' },
-      { provide: FIRESTORE_SETTINGS, useValue: { ignoreUndefinedProperties: true, merge: true } },
-      {
-        provide: USE_FIRESTORE_EMULATOR,
-        useValue: environment.firebase.useEmulators ? ['localhost', 8081] : undefined,
-      },
-      provideHttpClient(withInterceptorsFromDi()),
-    ],
-  }).catch((err) => console.log(err));
-});
+        // localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }),
+      });
+      if (environment.firebase.useEmulators) {
+        connectFirestoreEmulator(firestore, 'localhost', 8081);
+      }
+      return firestore;
+    }),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    { provide: FIRESTORE_SETTINGS, useValue: { ignoreUndefinedProperties: true, merge: true } },
+    {
+      provide: USE_FIRESTORE_EMULATOR,
+      useValue: environment.firebase.useEmulators ? ['localhost', 8081] : undefined,
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
+}).catch((err) => console.log(err));
