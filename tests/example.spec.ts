177b08f10199400c86ac36d3becbf0d4f2358f7c
Yudi <12040397+Yudi@users.noreply.github.com>
2024-06-01 15:05:29 -0300

    a

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://localhost:8100/');
+  await page.goto('https://fctapp.yudi.me/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/FCT App/);
