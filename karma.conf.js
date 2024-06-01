440ef2b2356b0609bca060dd28d65a7a133761d4
Yudi <12040397+Yudi@users.noreply.github.com>
2024-06-01 14:19:18 -0300

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/ngv'),
       subdir: '.',
-      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      reporters: [{ type: 'html' }, { type: 'text-summary' }, [{ type: 'lcov' }]],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
