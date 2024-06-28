d45f31a3c160ba15ca39e9780bdc74eecc14178c
Yudi <12040397+Yudi@users.noreply.github.com>
2024-06-28 10:52:28 -0300

    feat(frontend): add privacy.page.spec.ts

--- frontend/src/app/settings/privacy/privacy.page.spec.ts
+++ frontend/src/app/settings/privacy/privacy.page.spec.ts
@@ -14,4 +14,42 @@ describe('PrivacyPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should disable analytics', () => {
+    component.isAnalyticsEnabled = true;
+    localStorage.setItem('disable-analytics', '');
+
+    component.toggleAnalytics();
+
+    expect(component.isAnalyticsEnabled).toBe(false);
+    expect(localStorage.getItem('disable-analytics')).toBe('true');
+  });
+
+  it('should enable analytics', () => {
+    component.isAnalyticsEnabled = false;
+    localStorage.setItem('disable-analytics', 'true');
+
+    component.toggleAnalytics();
+
+    expect(component.isAnalyticsEnabled).toBe(true);
+    expect(localStorage.getItem('disable-analytics')).toBe('');
+  });
+
+  it('should disable monitoring', () => {
+    component.isMonitoringEnabled = true;
+
+    component.toggleMonitoring();
+
+    expect(component.isMonitoringEnabled).toBe(false);
+    expect(localStorage.getItem('disable-monitoring')).toBe('true');
+  });
+
+  it('should enable monitoring', () => {
+    component.isMonitoringEnabled = false;
+
+    component.toggleMonitoring();
+
+    expect(component.isMonitoringEnabled).toBe(true);
+    expect(localStorage.getItem('disable-monitoring')).toBe('');
+  });
 });
