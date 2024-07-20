ee43d0b32b4bae5486c383ba5c99c560fa6ea4ff
Willian Murayama <willianyos13@gmail.com>
2024-07-20 11:59:53 -0300

    feat(backend): caching requests

--- backend/src/weather/weather.controller.ts
+++ backend/src/weather/weather.controller.ts
@@ -6,10 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { WeatherService } from './weather.service';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('weather')
+@UseInterceptors(CacheInterceptor)
 export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
--- backend/src/weather/weather.module.ts
+++ backend/src/weather/weather.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import { WeatherController } from './weather.controller';
-
+import { CacheModule } from '@nestjs/cache-manager';
 @Module({
+  imports: [CacheModule.register({ ttl: 100000, max: 1 })],
+  // ttl in milliseconds
+  // max is max itens stored in cache
   controllers: [WeatherController],
   providers: [WeatherService],
 })
