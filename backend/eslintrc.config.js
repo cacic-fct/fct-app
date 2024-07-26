54c5d2f5256b4c84e0d93471a5fc54270c323acd
Yudi <12040397+Yudi@users.noreply.github.com>
2024-07-26 14:40:52 -0300

    feat(backend): setup lint

---
+++ backend/eslintrc.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+    ],
+    rules: {},
+  },
+  {
+    ignores: ['.eslintrc.js'],
+  },
+);
