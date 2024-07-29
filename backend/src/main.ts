e0f82b08c8cc49e18e9278439cdb190340994484
Willian Murayama <willianyos13@gmail.com>
2024-07-28 21:22:14 -0300

    feat(backend): enable CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.use(cookieParser());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
