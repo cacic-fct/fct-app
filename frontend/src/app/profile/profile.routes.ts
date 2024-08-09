79cd1863d91ac35bfa20c7f8a022771a5cbe762b
Yudi <12040397+Yudi@users.noreply.github.com>
2024-08-08 22:12:02 -0300

    feat: remove id code

--- frontend/src/app/profile/profile.routes.ts
+++ frontend/src/app/profile/profile.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 
 import { ProfilePage } from './profile.page';
-import { canActivate } from '@angular/fire/compat/auth-guard';
-import { redirectUnauthorizedToLogin } from 'src/app/shared/services/routing/guards.service';
 
 export const routes: Routes = [
   {
@@ -16,11 +14,5 @@ export const routes: Routes = [
     loadComponent: () => import('./wallet/wallet.page').then((m) => m.WalletPage),
     // TODO: Commented out until we fix data being overwritten
     // ...canActivate(redirectUnauthorizedToLogin),
-  },
-  {
-    path: 'codigo',
-    title: 'Código de identificação',
-    loadComponent: () => import('./id-code/id-code.page').then((m) => m.IdCodePage),
-    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
