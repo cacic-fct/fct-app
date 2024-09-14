93cc7a0a1e6333845c3d81aacb91943dde3eeacf
Yudi <12040397+Yudi@users.noreply.github.com>
2024-09-14 14:50:33 -0300

    feat: remove prettier astro plugins

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,13 +3,6 @@ export default {
   singleQuote: true,
   bracketSameLine: true,
   printWidth: 120,
-  plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
-  overrides: [
-    {
-      files: '*.astro',
-      options: {
-        parser: 'astro',
-      },
-    },
-  ],
+  plugins: [],
+  overrides: [],
 };
