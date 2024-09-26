831ec751f7c7fc082456de8bc764bfc760e0b53b
Yudi <12040397+Yudi@users.noreply.github.com>
2024-09-26 16:23:37 -0300

    feat: temporarily remove calendar route

--- frontend/src/app/tabs/tabs.routes.ts
+++ frontend/src/app/tabs/tabs.routes.ts
@@ -14,12 +14,7 @@ export const routes: Routes = [
         loadComponent: () => import('src/app/landing/landing.page').then((m) => m.LandingPage),
         ...canActivate(redirectLoggedInToEvents),
       },
-      {
-        path: 'calendario',
-        title: 'Calendário de eventos',
-        data: { preload: true },
-        loadChildren: () => import('src/app/tabs/calendar/calendar.routes').then((m) => m.routes),
-      },
+
       {
         path: 'eventos',
         title: 'Lista de eventos',
