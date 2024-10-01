1e214ae35e672c4ce15b0db50912b2c8dd453562
Yudi <12040397+Yudi@users.noreply.github.com>
2024-10-01 09:38:27 -0300

    feat: production barcode scanner debug

--- frontend/src/app/restricted-area/restricted-area.routes.ts
+++ frontend/src/app/restricted-area/restricted-area.routes.ts
@@ -28,4 +28,10 @@ export const routes: Routes = [
     title: 'Gerenciar eventos',
     loadChildren: () => import('./manage-events/manage-events.routes').then((m) => m.routes),
   },
+  {
+    path: 'leitor-codigo-barras',
+    title: 'Leitor de código de barras',
+    loadComponent: () =>
+      import('../development-tools/barcode-scanner/barcode-scanner.page').then((m) => m.BarcodeScannerPage),
+  },
 ];
