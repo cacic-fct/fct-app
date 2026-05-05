2650ece1b6956f4f11634e3a90bf23c994f6ca42
Yudi <12040397+Yudi@users.noreply.github.com>
2026-05-05 18:15:53 -0300

    Render dynamic routes on Server

--- apps/public/src/app/app.routes.server.ts
+++ apps/public/src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'event/:eventId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'profile/attendances/:eventType/:eventId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'validate/:certificateId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'validar/:certificateId',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
